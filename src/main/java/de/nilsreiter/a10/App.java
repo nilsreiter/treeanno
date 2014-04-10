9af0926dd6e8049db65e817bafbdb4fdaa810ea0
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-04-10 07:20:27 +0000

    Initial Maven project
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@11 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

---
+++ src/main/java/de/nilsreiter/a10/App.java
@@ -0,0 +1,13 @@
+package de.nilsreiter.a10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
