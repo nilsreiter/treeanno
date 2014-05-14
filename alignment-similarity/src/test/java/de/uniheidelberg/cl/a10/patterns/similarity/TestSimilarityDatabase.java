b041bd6d044ad4e6a9bb95796e517b160dbe33db
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-05-14 15:51:06 +0000

    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@67 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- alignment-similarity/src/test/java/de/uniheidelberg/cl/a10/patterns/similarity/TestSimilarityDatabase.java
+++ alignment-similarity/src/test/java/de/uniheidelberg/cl/a10/patterns/similarity/TestSimilarityDatabase.java
@@ -1,5 +1,6 @@
 package de.uniheidelberg.cl.a10.patterns.similarity;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -9,32 +10,65 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.nilsreiter.event.similarity.Random;
 import de.nilsreiter.util.db.DatabaseConfiguration;
-import de.uniheidelberg.cl.a10.HasGlobalId;
+import de.uniheidelberg.cl.a10.data2.Document;
+import de.uniheidelberg.cl.a10.data2.Token;
 
 public class TestSimilarityDatabase {
 
-	HasGlobalId[] events;
+	Token[] events;
+	Document[] documents;
+	SimilarityDatabase<Token> sd;
 
 	@Before
 	public void setUp() throws Exception {
-		events = new HasGlobalId[10];
-		for (int i = 0; i < 10; i++) {
-			events[i] = mock(HasGlobalId.class);
-			when(events[i].getGlobalId()).thenReturn(String.valueOf(i));
+		documents = new Document[2];
+		for (int i = 0; i < 2; i++) {
+			documents[i] = mock(Document.class);
+			when(documents[i].getId()).thenReturn("doc" + i);
 		}
+
+		events = new Token[10];
+		for (int i = 0; i < 10; i++) {
+			events[i] = mock(Token.class);
+			when(events[i].getId()).thenReturn(String.valueOf(i));
+			when(events[i].getRitualDocument()).thenReturn(documents[i % 2]);
+		}
+		SimilarityDatabase.TBL_DOCUMENTS = "documents_test";
+		SimilarityDatabase.TBL_SIMILARITIES = "similarities_test";
+		sd = new SimilarityDatabase<Token>(
+				DatabaseConfiguration.getDefaultConfiguration());
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		sd.getConnection().close();
 	}
 
 	@Test
 	public void test() throws ClassNotFoundException, SQLException {
-		SimilarityDatabase sd = new SimilarityDatabase(
-				DatabaseConfiguration.getDefaultConfiguration());
-		sd.initTable();
-		sd.putSimilarity(Null.class, events[0], events[1], 0.5);
-	}
+		sd.rebuild();
+		sd.putSimilarity(Null.class, events[0], events[1], 0.1);
+		sd.putSimilarity(Random.class, events[1], events[1], 0.2);
+		sd.putSimilarity(Null.class, events[2], events[1], 0.3);
+		sd.putSimilarity(Null.class, events[3], events[1], 0.4);
+		sd.putSimilarity(Null.class, events[4], events[0], 0.5);
+		sd.putSimilarity(Null.class, events[5], events[2], 0.6);
 
+		assertEquals(0.1, sd.getSimilarity(Null.class, events[0], events[1]),
+				0.0);
+		assertEquals(0.2, sd.getSimilarity(Random.class, events[1], events[1]),
+				0.0);
+		assertEquals(0.3, sd.getSimilarity(Null.class, events[2], events[1]),
+				0.0);
+		assertEquals(0.4, sd.getSimilarity(Null.class, events[3], events[1]),
+				0.0);
+		assertEquals(0.5, sd.getSimilarity(Null.class, events[4], events[0]),
+				0.0);
+		assertEquals(0.6, sd.getSimilarity(Null.class, events[5], events[2]),
+				0.0);
+
+	}
 }
