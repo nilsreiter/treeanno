cf69615662d4f5fae2719e1466d33f310ea2ba7f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-05-20 14:30:42 +0000

    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@69 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- de.pado.sigf/src/main/java/de/pado/sigf/AverageART.java
+++ de.pado.sigf/src/main/java/de/pado/sigf/AverageART.java
@@ -10,93 +10,106 @@
 
 package de.pado.sigf;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Vector;
 
-import java.util.*;
-import java.io.*;
-
-/** Compute average for observations.
-    Each observation is a single Long.
-**/
-
-
+/**
+ * Compute average for observations. Each observation is a single Long.
+ **/
 
 class Average implements Statistic<Singleton<Long>> {
-   
-                   // sufficient statistics:
-    Long sum;      // sum of all observations
-    Long no;       // number of observations
 
-  public double compute() {
-    return (((double) sum)/no); 
-    }
-    
-  public void add_observation(Singleton<Long> observation) {	 
-	  sum += observation.first();
-	  no +=1;
-  }
+	// sufficient statistics:
+	Long sum; // sum of all observations
+	Long no; // number of observations
 
-  public Average clone() {
-	  return new Average(sum,no); 
-  }
+	@Override
+	public double compute() {
+		return (((double) sum) / no);
+	}
 
-  Average() {
-    sum = new Long(0);
-    no = new Long(0);
-  }
+	@Override
+	public void add_observation(Singleton<Long> observation) {
+		sum += observation.first();
+		no += 1;
+	}
 
-  Average(Long counter, Long noObs) {
-    sum = counter;
-    no = noObs;
-  }
+	@Override
+	public Average clone() {
+		return new Average(sum, no);
+	}
 
-  public long num_observations() {
-	return no;
-    }
+	Average() {
+		sum = new Long(0);
+		no = new Long(0);
+	}
+
+	Average(Long counter, Long noObs) {
+		sum = counter;
+		no = noObs;
+	}
+
+	@Override
+	public long num_observations() {
+		return no;
+	}
 
 }
 
 /**
- * Computation of Averages for two datasets and comparison with a randomization test
- * This class extends the (abstract) ApproximateRandomizationTest class. It only 
- * implements main(), readInput(), and calls the Average class.
+ * Computation of Averages for two datasets and comparison with a randomization
+ * test This class extends the (abstract) ApproximateRandomizationTest class. It
+ * only implements main(), readInput(), and calls the Average class.
  * 
  * @author pado
- *
+ * 
  */
 public class AverageART extends ApproximateRandomizationTest<Singleton<Long>> {
-  
-    /**
-     * Read a vector of Long[1] (individual observations in Arrays) from a file.
-     */
-  public Vector<Singleton<Long>> readInput(String filename) throws IOException, FileNotFoundException {
 
-    Vector<Singleton<Long>> v = new Vector<Singleton<Long>>();
-    BufferedReader fileReader = new BufferedReader(new FileReader(filename));
-    String line;
-    while ((line = fileReader.readLine()) != null) {
-        if (line.length() == 0)
-          continue;
-        Long obs = Long.valueOf(line);        
-        Singleton<Long> longArray = new Singleton<Long>(obs);
-        v.addElement(longArray);
-    }
-    return v;
-  }
-  
-  /** 
-   * Computation of Averages for two datasets and comparison with a randomization test.
-   * @param <filename1>, <filename2>, <number of iterations>
-   * @throws ClassNotFoundException
-   * @throws InstantiationException
-   * @throws IllegalAccessException
-   */
-  public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-    if (args.length != 3) {
-      System.err.println("Usage: AverageART <file1> <file2> <no. of iterations>");
-      System.exit(1);
-    }    
-    ApproximateRandomizationTest<Singleton<Long>> art = new AverageART();
-    art.run(args[0], args[1], Average.class, Long.valueOf(args[2]).longValue()); 
-  }
-  
+	/**
+	 * Read a vector of Long[1] (individual observations in Arrays) from a file.
+	 */
+	@Override
+	public Vector<Singleton<Long>> readInput(InputStream filename)
+			throws IOException, FileNotFoundException {
+
+		Vector<Singleton<Long>> v = new Vector<Singleton<Long>>();
+		BufferedReader fileReader = new BufferedReader(new InputStreamReader(
+				filename));
+		String line;
+		while ((line = fileReader.readLine()) != null) {
+			if (line.length() == 0)
+				continue;
+			Long obs = Long.valueOf(line);
+			Singleton<Long> longArray = new Singleton<Long>(obs);
+			v.addElement(longArray);
+		}
+		return v;
+	}
+
+	/**
+	 * Computation of Averages for two datasets and comparison with a
+	 * randomization test.
+	 * 
+	 * @param <filename1>, <filename2>, <number of iterations>
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public static void main(String[] args) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
+		if (args.length != 3) {
+			System.err
+					.println("Usage: AverageART <file1> <file2> <no. of iterations>");
+			System.exit(1);
+		}
+		ApproximateRandomizationTest<Singleton<Long>> art = new AverageART();
+		art.run(args[0], args[1], Average.class, Long.valueOf(args[2])
+				.longValue());
+	}
+
 }
