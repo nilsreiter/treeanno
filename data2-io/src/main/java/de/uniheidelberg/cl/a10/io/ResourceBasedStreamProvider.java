0c209eb68e1432bcebb748769b348e187a479893
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-06-25 09:12:20 +0000

    Added ResourceBasedStreamProvider that can be used in tests
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@89 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

---
+++ data2-io/src/main/java/de/uniheidelberg/cl/a10/io/ResourceBasedStreamProvider.java
@@ -0,0 +1,22 @@
+package de.uniheidelberg.cl.a10.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import de.uniheidelberg.cl.a10.api.DataStreamProvider;
+
+public class ResourceBasedStreamProvider implements DataStreamProvider {
+
+	String suffix = ".xml";
+
+	@Override
+	public InputStream findStreamFor(String objectName) throws IOException {
+		if (!objectName.endsWith(suffix)) {
+			objectName = objectName + suffix;
+		}
+		return Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(objectName);
+
+	}
+
+}
