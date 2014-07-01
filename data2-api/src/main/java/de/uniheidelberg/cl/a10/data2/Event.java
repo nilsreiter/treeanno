8f97f1e8ab40957a8c91b96cdbbbf008734775db
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-07-01 09:28:27 +0000

    Event now implements HasTokens
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@95 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- data2-api/src/main/java/de/uniheidelberg/cl/a10/data2/Event.java
+++ data2-api/src/main/java/de/uniheidelberg/cl/a10/data2/Event.java
@@ -3,7 +3,7 @@ package de.uniheidelberg.cl.a10.data2;
 import java.util.List;
 import java.util.Map;
 
-public interface Event extends AnnotationObjectInDocument {
+public interface Event extends AnnotationObjectInDocument, HasTokens {
 	Map<String, List<? extends AnnotationObjectInDocument>> getArguments();
 
 	void setArguments(
