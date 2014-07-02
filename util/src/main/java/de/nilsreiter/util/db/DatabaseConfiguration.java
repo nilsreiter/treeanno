3fb7734708731f73a40876ad1cfea8bb5371a520
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-07-02 14:11:46 +0000

    Added a lot of setter methods
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@102 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- util/src/main/java/de/nilsreiter/util/db/DatabaseConfiguration.java
+++ util/src/main/java/de/nilsreiter/util/db/DatabaseConfiguration.java
@@ -38,6 +38,46 @@ public class DatabaseConfiguration {
 		return b.toString();
 	}
 
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	public String getProtocol() {
+		return protocol;
+	}
+
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getUsername() {
 		return username;
 	}
