fbbf2b298ae56c1af354378bc88206821ea8c111
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-07-04 07:31:39 +0000

    Indentation
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@109 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- de.saar.coli.salsa.reiter.framenet/src/main/java/de/saar/coli/salsa/reiter/framenet/fncorpus/AnnotationCorpus.java
+++ de.saar.coli.salsa.reiter.framenet/src/main/java/de/saar/coli/salsa/reiter/framenet/fncorpus/AnnotationCorpus.java
@@ -24,160 +24,159 @@ import de.saar.coli.salsa.reiter.framenet.LexicalUnit;
  */
 public abstract class AnnotationCorpus extends CorpusReader {
 
-    int maximalId = 0;
+	int maximalId = 0;
 
-    /**
-     * An index of AnnotatedLexicalUnits.
-     */
-    private Map<LexicalUnit, AnnotatedLexicalUnit> annotations = null;
+	/**
+	 * An index of AnnotatedLexicalUnits.
+	 */
+	private Map<LexicalUnit, AnnotatedLexicalUnit> annotations = null;
 
-    /**
-     * A constructor, just as in FrameNetCorpus.
-     * 
-     * @param frameNet
-     *            The FrameNet object
-     * @param logger
-     *            The logger
-     */
-    public AnnotationCorpus(final FrameNet frameNet, final Logger logger) {
-	super(frameNet, logger);
-	annotations = new HashMap<LexicalUnit, AnnotatedLexicalUnit>();
-    }
-
-    /**
-     * Returns the annotated examples with the given lexical unit. If no
-     * annotation has been found, the method returns null.
-     * 
-     * @param lu
-     *            The lexical unit
-     * @return An object of the class AnnotatedLexicalUnit
-     */
-    public AnnotatedLexicalUnit getAnnotation(final LexicalUnit lu) {
-	try {
-	    return annotations.get(lu);
-	} catch (NullPointerException e) {
-	    return null;
+	/**
+	 * A constructor, just as in FrameNetCorpus.
+	 * 
+	 * @param frameNet
+	 *            The FrameNet object
+	 * @param logger
+	 *            The logger
+	 */
+	public AnnotationCorpus(final FrameNet frameNet, final Logger logger) {
+		super(frameNet, logger);
+		annotations = new HashMap<LexicalUnit, AnnotatedLexicalUnit>();
 	}
-    }
 
-    @Override
-    public void parse(final File directory) {
-	parse(directory, ".xml");
-    }
-
-    /**
-     * An extension of {@link #parse(File)}. Allows the specification of a
-     * pattern, which must be contained in the filename of the files to be
-     * loaded.
-     * 
-     * Example: If you specify "2442" as pattern string, only lu-files with 2442
-     * in their names will be loaded.
-     * 
-     * @param directory
-     *            The luXML directory
-     * @param pattern
-     *            A pattern
-     */
-    public void parse(final File directory, final String pattern) {
-	if (directory.isDirectory() && directory.canRead()) {
-	    for (File file : directory.listFiles()) {
-		if (file.getName().contains(pattern)) {
-		    parseFile(file);
+	/**
+	 * Returns the annotated examples with the given lexical unit. If no
+	 * annotation has been found, the method returns null.
+	 * 
+	 * @param lu
+	 *            The lexical unit
+	 * @return An object of the class AnnotatedLexicalUnit
+	 */
+	public AnnotatedLexicalUnit getAnnotation(final LexicalUnit lu) {
+		try {
+			return annotations.get(lu);
+		} catch (NullPointerException e) {
+			return null;
 		}
-	    }
 	}
-    }
 
-    /**
-     * An extension of {@link #parse(File)}. Allows the specification of a list
-     * of file names to be excluded.
-     * 
-     * Example: If you specify "lu2442.xml", every file except lu2442.xml will
-     * be loaded.
-     * 
-     * @param directory
-     *            The luXML directory
-     * @param names
-     *            A list of file names
-     */
-    public void parseWithout(final File directory, final String... names) {
-	if (directory.isDirectory() && directory.canRead()) {
-	    for (File file : directory.listFiles(new FilenameFilter() {
+	@Override
+	public void parse(final File directory) {
+		parse(directory, ".xml");
+	}
 
-		@Override
-		public boolean accept(final File arg0, final String arg1) {
-		    // TODO Auto-generated method stub
-		    return arg1.endsWith(".xml") && !contains(names, arg1);
+	/**
+	 * An extension of {@link #parse(File)}. Allows the specification of a
+	 * pattern, which must be contained in the filename of the files to be
+	 * loaded.
+	 * 
+	 * Example: If you specify "2442" as pattern string, only lu-files with 2442
+	 * in their names will be loaded.
+	 * 
+	 * @param directory
+	 *            The luXML directory
+	 * @param pattern
+	 *            A pattern
+	 */
+	public void parse(final File directory, final String pattern) {
+		if (directory.isDirectory() && directory.canRead()) {
+			for (File file : directory.listFiles()) {
+				if (file.getName().contains(pattern)) {
+					parseFile(file);
+				}
+			}
 		}
-	    })) {
-		parseFile(file);
-	    }
 	}
-    }
 
-    public static boolean contains(final String[] array, final String key) {
-	for (String a : array) {
-	    if (a.equals(key)) {
-		return true;
-	    }
+	/**
+	 * An extension of {@link #parse(File)}. Allows the specification of a list
+	 * of file names to be excluded.
+	 * 
+	 * Example: If you specify "lu2442.xml", every file except lu2442.xml will
+	 * be loaded.
+	 * 
+	 * @param directory
+	 *            The luXML directory
+	 * @param names
+	 *            A list of file names
+	 */
+	public void parseWithout(final File directory, final String... names) {
+		if (directory.isDirectory() && directory.canRead()) {
+			for (File file : directory.listFiles(new FilenameFilter() {
+
+				@Override
+				public boolean accept(final File arg0, final String arg1) {
+					return arg1.endsWith(".xml") && !contains(names, arg1);
+				}
+			})) {
+				parseFile(file);
+			}
+		}
 	}
-	return false;
-    }
 
-    /**
-     * Parses the file.
-     * 
-     * @param file
-     *            The file to parse
-     */
-    protected abstract void parseFile(File file);
-
-    /**
-     * Adds an annotated lexical unit to the index of lexical units.
-     * 
-     * @param lu
-     *            The lexical unit
-     * @param alu
-     *            The annotated lexical unit
-     */
-    public void addAnnotation(final LexicalUnit lu,
-	    final AnnotatedLexicalUnit alu) {
-	annotations.put(lu, alu);
-	if (alu.getId() < 0) {
-	    alu.setId(getNextId());
-	} else {
-	    if (alu.getId() > maximalId) {
-		maximalId = alu.getId();
-	    }
+	public static boolean contains(final String[] array, final String key) {
+		for (String a : array) {
+			if (a.equals(key)) {
+				return true;
+			}
+		}
+		return false;
 	}
-    }
 
-    private int getNextId() {
-	return ++maximalId;
-    }
+	/**
+	 * Parses the file.
+	 * 
+	 * @param file
+	 *            The file to parse
+	 */
+	protected abstract void parseFile(File file);
 
-    /**
-     * @return
-     * @see java.util.Map#keySet()
-     */
-    public Set<LexicalUnit> getLexicalUnits() {
-	return annotations.keySet();
-    }
+	/**
+	 * Adds an annotated lexical unit to the index of lexical units.
+	 * 
+	 * @param lu
+	 *            The lexical unit
+	 * @param alu
+	 *            The annotated lexical unit
+	 */
+	public void addAnnotation(final LexicalUnit lu,
+			final AnnotatedLexicalUnit alu) {
+		annotations.put(lu, alu);
+		if (alu.getId() < 0) {
+			alu.setId(getNextId());
+		} else {
+			if (alu.getId() > maximalId) {
+				maximalId = alu.getId();
+			}
+		}
+	}
 
-    /**
-     * @return
-     * @see java.util.Map#size()
-     */
-    public int getNumberOfAnnotations() {
-	return annotations.size();
-    }
+	private int getNextId() {
+		return ++maximalId;
+	}
 
-    /**
-     * @return
-     * @see java.util.Map#values()
-     */
-    public Collection<AnnotatedLexicalUnit> getAnnotations() {
-	return annotations.values();
-    }
+	/**
+	 * @return
+	 * @see java.util.Map#keySet()
+	 */
+	public Set<LexicalUnit> getLexicalUnits() {
+		return annotations.keySet();
+	}
+
+	/**
+	 * @return
+	 * @see java.util.Map#size()
+	 */
+	public int getNumberOfAnnotations() {
+		return annotations.size();
+	}
+
+	/**
+	 * @return
+	 * @see java.util.Map#values()
+	 */
+	public Collection<AnnotatedLexicalUnit> getAnnotations() {
+		return annotations.values();
+	}
 
 }
