e8e90cbcbfcd2f3d0c0cbd5087835956aa52573d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-08-01 10:42:24 +0000

    Added parameters and stuff
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@144 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- alignment-api/src/main/java/de/nilsreiter/alignment/algorithm/BayesianModelMerging.java
+++ alignment-api/src/main/java/de/nilsreiter/alignment/algorithm/BayesianModelMerging.java
@@ -1,6 +1,11 @@
 package de.nilsreiter.alignment.algorithm;
 
+import de.uniheidelberg.cl.a10.patterns.similarity.SimilarityFunction;
+
 public interface BayesianModelMerging<T> extends AlignmentAlgorithm<T> {
-	public static final String PARAM_THRESHOLD = "Threshold";
+	public static final String CONFIG_THRESHOLD = "Threshold";
+	public static final String CONFIG_THREADED = "Threaded";
+
+	SimilarityFunction<T> getSimilarityFunction();
 
 }
