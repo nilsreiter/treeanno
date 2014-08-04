a727016006a26b8ce7dd83b26dc60a34ec0faaaa
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-08-04 08:27:10 +0000

    This is not needed anymore, therefore deprecated
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@156 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- alignment-algorithms/src/main/java/de/uniheidelberg/cl/a10/patterns/mroth/impl/MRSystemFactory.java
+++ alignment-algorithms/src/main/java/de/uniheidelberg/cl/a10/patterns/mroth/impl/MRSystemFactory.java
@@ -2,13 +2,15 @@ package de.uniheidelberg.cl.a10.patterns.mroth.impl;
 
 import java.io.FileNotFoundException;
 
+import de.nilsreiter.alignment.algorithm.MRSystem;
+import de.nilsreiter.alignment.algorithm.impl.MRSystem_impl;
 import de.uniheidelberg.cl.a10.data2.FrameTokenEvent;
 import de.uniheidelberg.cl.a10.patterns.data.Probability;
-import de.uniheidelberg.cl.a10.patterns.mroth.MRSystem;
 import de.uniheidelberg.cl.a10.patterns.similarity.SimilarityConfiguration;
 import de.uniheidelberg.cl.a10.patterns.similarity.SimilarityFunction;
 import de.uniheidelberg.cl.a10.patterns.similarity.SimilarityFunctionFactory;
 
+@Deprecated
 public class MRSystemFactory {
 
 	public static <T extends FrameTokenEvent> MRSystem<T> getInstance(
@@ -16,7 +18,8 @@ public class MRSystemFactory {
 			SecurityException, InstantiationException, IllegalAccessException,
 			ClassNotFoundException {
 		MRSystem_impl<T> mrs = new MRSystem_impl<T>();
-		SimilarityFunctionFactory<T> sfunfact = new SimilarityFunctionFactory<T>();
+		SimilarityFunctionFactory<T> sfunfact =
+				new SimilarityFunctionFactory<T>();
 		mrs.setSimilarityFunction(sfunfact.getSimilarityFunction(sConf));
 		mrs.setThreshold(Probability.fromProbability(sConf.getThreshold()));
 		return mrs;
