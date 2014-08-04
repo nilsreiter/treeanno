fc44bd13ff60ee6af166f65d15857e200e4d7029
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-08-04 08:22:32 +0000

    FrameTokenEvent is now deprecated
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@150 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- data2-api/src/main/java/de/uniheidelberg/cl/a10/data2/FrameTokenEvent.java
+++ data2-api/src/main/java/de/uniheidelberg/cl/a10/data2/FrameTokenEvent.java
@@ -3,7 +3,9 @@ package de.uniheidelberg.cl.a10.data2;
 import de.uniheidelberg.cl.a10.HasId;
 import de.uniheidelberg.cl.a10.HasTarget;
 
-public interface FrameTokenEvent extends HasTarget, HasId, AnnotationObjectInDocument {
+@Deprecated
+public interface FrameTokenEvent extends HasTarget, HasId,
+		AnnotationObjectInDocument {
 	Source source();
 
 	public enum Source {
