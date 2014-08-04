eb25d7553a3010c05a7685c4f148e654edc44782
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-08-04 08:23:09 +0000

    deprecated
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@151 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- data2-core/src/main/java/de/uniheidelberg/cl/a10/data2/impl/FrameEvent_impl.java
+++ data2-core/src/main/java/de/uniheidelberg/cl/a10/data2/impl/FrameEvent_impl.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import de.uniheidelberg.cl.a10.data2.AnnotationObjectInDocument;
 import de.uniheidelberg.cl.a10.data2.Frame;
 
+@Deprecated
 public class FrameEvent_impl extends FrameTokenEvent_impl {
 
 	Frame frame = null;
@@ -12,8 +13,9 @@ public class FrameEvent_impl extends FrameTokenEvent_impl {
 	public FrameEvent_impl(final Frame frame) {
 		super(frame.getId(), frame.getTarget());
 		this.frame = frame;
-		this.position = (double) frame.indexOf()
-				/ (double) frame.getRitualDocument().getFrames().size();
+		this.position =
+				(double) frame.indexOf()
+						/ (double) frame.getRitualDocument().getFrames().size();
 		this.ritualDocument = frame.getRitualDocument();
 	}
 
@@ -30,6 +32,7 @@ public class FrameEvent_impl extends FrameTokenEvent_impl {
 	/**
 	 * @return the frame
 	 */
+	@Override
 	public Frame getFrame() {
 		return frame;
 	}
