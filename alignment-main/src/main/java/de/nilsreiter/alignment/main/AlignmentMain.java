9c788df1824cb55bb628afe1c19b9702b7bd582f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-08-05 07:34:03 +0000

    Removed all optiony
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@200 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- alignment-main/src/main/java/de/nilsreiter/alignment/main/AlignmentMain.java
+++ alignment-main/src/main/java/de/nilsreiter/alignment/main/AlignmentMain.java
@@ -3,13 +3,8 @@ package de.nilsreiter.alignment.main;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.commons.configuration.BaseConfiguration;
-import org.apache.commons.configuration.Configuration;
-import org.kohsuke.args4j.Option;
-
 import de.nilsreiter.alignment.algorithm.AlgorithmFactory;
 import de.nilsreiter.alignment.algorithm.AlignmentAlgorithm;
-import de.nilsreiter.alignment.algorithm.NeedlemanWunsch;
 import de.uniheidelberg.cl.a10.data2.Event;
 import de.uniheidelberg.cl.a10.data2.alignment.Alignment;
 import de.uniheidelberg.cl.a10.data2.alignment.io.AlignmentWriter;
@@ -17,34 +12,22 @@ import de.uniheidelberg.cl.a10.main.MainWithInputDocuments;
 
 public class AlignmentMain extends MainWithInputDocuments {
 
-	@Option(name = "--algorithm",
-			usage = "fully specified class name of algorihm")
-	String algorithmClass = null;
-
 	public static void main(String[] args) throws IOException {
 		AlignmentMain r = new AlignmentMain();
 		r.processArguments(args);
+		r.init();
 		r.run();
 	}
 
-	public AlignmentMain() {
-		super();
+	private void init() {
 
 	}
 
 	private void run() throws IOException {
+
 		AlgorithmFactory af = AlgorithmFactory.getInstance();
-		Configuration configuration = new BaseConfiguration();
-		configuration.setProperty(AlgorithmFactory.CONFIG_KEY_ALGORITHM,
-				algorithmClass);
-		configuration.setProperty("database.url",
-				"jdbc:mysql://127.0.0.1:53903/reiter");
-		configuration.setProperty("database.username", "reiterns");
-		configuration.setProperty("database.password", "bybNoaKni");
-		configuration.setProperty(NeedlemanWunsch.PARAM_THRESHOLD, "0.5");
-		configuration.setProperty(
-				"similarity.de.nilsreiter.event.similarity.WordNet", "1.0");
-		AlignmentAlgorithm<Event> algo = af.getAlgorithm(configuration);
+		AlignmentAlgorithm<Event> algo =
+				af.getAlgorithm(this.getConfiguration());
 		List<Event> l1 = this.getDocuments().get(0).getEvents();
 		List<Event> l2 = this.getDocuments().get(1).getEvents();
 		Alignment<Event> alignment = algo.align(l1, l2);
