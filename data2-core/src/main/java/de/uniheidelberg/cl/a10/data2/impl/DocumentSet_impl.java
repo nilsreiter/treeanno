7b23520e98485517ba20603db61a840f5167ba4f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-09-09 13:48:50 +0000

    Document sets now have a defined order
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@238 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- data2-core/src/main/java/de/uniheidelberg/cl/a10/data2/impl/DocumentSet_impl.java
+++ data2-core/src/main/java/de/uniheidelberg/cl/a10/data2/impl/DocumentSet_impl.java
@@ -1,6 +1,8 @@
 package de.uniheidelberg.cl.a10.data2.impl;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -53,11 +55,28 @@ public class DocumentSet_impl implements DocumentSet {
 
 	@Override
 	public boolean add(Document e) {
-		return set.add(e);
+		boolean b = set.add(e);
+		Collections.sort(set, new Comparator<Document>() {
+
+			@Override
+			public int compare(Document o1, Document o2) {
+				return o1.getId().compareTo(o2.getId());
+			}
+		});
+		return b;
 	}
 
+	@Override
 	public boolean addAll(Collection<? extends Document> c) {
-		return set.addAll(c);
+		boolean b = set.addAll(c);
+		Collections.sort(set, new Comparator<Document>() {
+
+			@Override
+			public int compare(Document o1, Document o2) {
+				return o1.getId().compareTo(o2.getId());
+			}
+		});
+		return b;
 	}
 
 }
