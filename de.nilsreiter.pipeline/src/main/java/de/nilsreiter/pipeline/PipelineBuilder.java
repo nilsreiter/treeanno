0a5936f107393cf9dbe4d65da6932ec7b08a7c96
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-09-25 14:43:58 +0000

    Added cwb() output
    git-svn-id: svn+ssh://zwergdrossel.ims.uni-stuttgart.de/projekte/clarin/reiterns/reiterns/a10/trunk@259 fc50e3fe-9590-427c-a3cf-f2939c79cb5b

--- de.nilsreiter.pipeline/src/main/java/de/nilsreiter/pipeline/PipelineBuilder.java
+++ de.nilsreiter.pipeline/src/main/java/de/nilsreiter/pipeline/PipelineBuilder.java
@@ -10,6 +10,7 @@ import org.apache.uima.resource.ResourceInitializationException;
 
 import de.nilsreiter.pipeline.uima.Data2Exporter;
 import de.tudarmstadt.ukp.dkpro.core.io.conll.Conll2006Writer;
+import de.tudarmstadt.ukp.dkpro.core.io.imscwb.ImsCwbWriter;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 
 public class PipelineBuilder {
@@ -23,7 +24,7 @@ public class PipelineBuilder {
 
 	public static List<AnalysisEngineDescription> xmi(
 			List<AnalysisEngineDescription> pipeline, File directory)
-					throws ResourceInitializationException {
+			throws ResourceInitializationException {
 		pipeline.add(createEngineDescription(XmiWriter.class,
 				XmiWriter.PARAM_TARGET_LOCATION,
 				new File(directory, "xmi").getAbsolutePath()));
@@ -32,16 +33,27 @@ public class PipelineBuilder {
 
 	public static List<AnalysisEngineDescription> conll(
 			List<AnalysisEngineDescription> pipeline, File directory)
-					throws ResourceInitializationException {
+			throws ResourceInitializationException {
 		pipeline.add(createEngineDescription(Conll2006Writer.class,
 				Conll2006Writer.PARAM_TARGET_LOCATION, new File(directory,
 						"conll").getAbsolutePath()));
 		return pipeline;
 	}
 
+	public static List<AnalysisEngineDescription> cwb(
+			List<AnalysisEngineDescription> pipeline, File directory)
+			throws ResourceInitializationException {
+		pipeline.add(createEngineDescription(ImsCwbWriter.class,
+				ImsCwbWriter.PARAM_TARGET_LOCATION,
+				new File(directory, "cwb").getAbsolutePath(),
+				ImsCwbWriter.PARAM_CQPWEB_COMPATIBILITY, true,
+				ImsCwbWriter.PARAM_WRITE_CPOS, false));
+		return pipeline;
+	}
+
 	public static List<AnalysisEngineDescription> data2(
 			List<AnalysisEngineDescription> pipeline, File directory)
-					throws ResourceInitializationException {
+			throws ResourceInitializationException {
 		pipeline.add(createEngineDescription(Data2Exporter.class,
 				Data2Exporter.PARAM_OUTPUT_DIRECTORY, new File(directory,
 						"data2").getAbsolutePath()));
