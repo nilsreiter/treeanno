71da56020191ccac9b60fda29d1218ab54e19dcf
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-10-14 20:56:08 +0200

    Now we can also produce an HTML representation on the server (faster)

--- de.nilsreiter.web/src/main/java/de/nilsreiter/web/rpc/GetDocument.java
+++ de.nilsreiter.web/src/main/java/de/nilsreiter/web/rpc/GetDocument.java
@@ -2,7 +2,9 @@ package de.nilsreiter.web.rpc;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,10 +14,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.hp.gagawa.java.elements.Div;
+import com.hp.gagawa.java.elements.Span;
+
+import de.nilsreiter.util.StringUtil;
 import de.nilsreiter.web.json.JSONConversion;
 import de.uniheidelberg.cl.a10.data2.Document;
 import de.uniheidelberg.cl.a10.data2.Event;
 import de.uniheidelberg.cl.a10.data2.Frame;
+import de.uniheidelberg.cl.a10.data2.Mention;
 import de.uniheidelberg.cl.a10.data2.Sentence;
 import de.uniheidelberg.cl.a10.data2.Token;
 
@@ -25,6 +32,11 @@ import de.uniheidelberg.cl.a10.data2.Token;
 public class GetDocument extends RPCServlet {
 	private static final long serialVersionUID = 1L;
 	Map<Document, JSONObject> jsonCache = new HashMap<Document, JSONObject>();
+	Map<Document, String> htmlCache = new HashMap<Document, String>();
+
+	enum DocumentFormat {
+		JSON, HTML
+	};
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -32,6 +44,74 @@ public class GetDocument extends RPCServlet {
 	 */
 	@Override
 	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		DocumentFormat format =
+				getArgument(request, DocumentFormat.class, "format",
+						DocumentFormat.JSON);
+		switch (format) {
+		case HTML:
+			this.doGetHTML(request, response);
+			break;
+		default:
+		case JSON:
+			this.doGetJSON(request, response);
+			break;
+		}
+
+	}
+
+	protected void doGetHTML(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		Document document = this.getDocument(request);
+		Div docDiv = new Div();
+		// docDiv.setCSSClass(document.getId());
+
+		Map<Token, Set<Event>> tokenEventMap = new HashMap<Token, Set<Event>>();
+		for (Event event : document.getEvents()) {
+			Token evoker = event.firstToken();
+			if (!tokenEventMap.containsKey(evoker))
+				tokenEventMap.put(evoker, new HashSet<Event>());
+			tokenEventMap.get(evoker).add(event);
+		}
+		for (Sentence sentence : document.getSentences()) {
+			Span sentenceSpan = new Span();
+			sentenceSpan.setCSSClass(sentence.getId() + " sentence");
+			int lastpos = 0;
+			for (Token token : sentence.getTokens()) {
+				Span tokenSpan = new Span();
+				Set<String> classes = new HashSet<String>();
+				classes.add("token");
+				classes.add(token.getId());
+				for (Frame frame : token.getFrames()) {
+					classes.add("frame");
+					classes.add(frame.getFrameName());
+				}
+				for (Mention mention : token.getMentions()) {
+					classes.add("mention");
+					classes.add(mention.getId());
+					classes.add(mention.getEntity().getId());
+				}
+				if (tokenEventMap.containsKey(token))
+					for (Event event : tokenEventMap.get(token)) {
+						classes.add("event");
+						classes.add(event.getId());
+						classes.add(event.getEventClass());
+					}
+				tokenSpan.setCSSClass(StringUtil.join(classes, " ", ""));
+				tokenSpan.appendText(token.getSurface());
+				if (token.getBegin() > lastpos) {
+					sentenceSpan.appendText(" ");
+				}
+				sentenceSpan.appendChild(tokenSpan);
+				lastpos = token.getEnd();
+			}
+			docDiv.appendChild(sentenceSpan);
+		}
+		returnHTML(response, docDiv.write());
+	}
+
+	protected void doGetJSON(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		Document document = this.getDocument(request);
 
@@ -72,6 +152,5 @@ public class GetDocument extends RPCServlet {
 		jsonCache.put(document, json);
 
 		returnJSON(response, json);
-
 	}
 }
