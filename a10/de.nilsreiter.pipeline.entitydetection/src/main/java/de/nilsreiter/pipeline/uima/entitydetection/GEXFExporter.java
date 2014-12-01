432de9329ae51782cbd7affcb449f73a2a813a07
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-12-01 12:23:33 +0100

    Now setting node labels

--- a10/de.nilsreiter.pipeline.entitydetection/src/main/java/de/nilsreiter/pipeline/uima/entitydetection/GEXFExporter.java
+++ a10/de.nilsreiter.pipeline.entitydetection/src/main/java/de/nilsreiter/pipeline/uima/entitydetection/GEXFExporter.java
@@ -38,7 +38,7 @@ public class GEXFExporter extends JCasConsumer_ImplBase {
 		Calendar date = Calendar.getInstance();
 
 		gexf.getMetadata().setLastModified(date.getTime())
-		.setCreator("Gephi.org");
+				.setCreator("Gephi.org");
 		Graph graph = gexf.getGraph();
 		graph.setDefaultEdgeType(EdgeType.UNDIRECTED).setMode(Mode.STATIC);
 
@@ -47,6 +47,7 @@ public class GEXFExporter extends JCasConsumer_ImplBase {
 			if (!nodeIndex.containsKey(entity.getIdentifier())) {
 				Node eNode = graph.createNode(entity.getIdentifier());
 				nodeIndex.put(entity.getIdentifier(), eNode);
+				eNode.setLabel(entity.getIdentifier());
 			}
 		}
 
