aaae0fbf623a7629961ce9a29c50ec45ecb3f370
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-12-04 18:22:06 +0100

    Class to clean an InputStream from PG-markings and write it into an OutputStream

---
+++ de.nilsreiter.pg/src/main/java/de/nilsreiter/pg/GutenbergClean.java
@@ -0,0 +1,53 @@
+package de.nilsreiter.pg;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import de.uniheidelberg.cl.a10.MainWithIO;
+
+public class GutenbergClean extends MainWithIO {
+
+	public static final String PG_MARK_START =
+			"*** START OF THIS PROJECT GUTENBERG EBOOK";
+	public static final String PG_MARK_END =
+			"*** END OF THIS PROJECT GUTENBERG EBOOK";
+
+	char lineSeparator = '\n';
+
+	public static void main(String[] args) throws IOException {
+		GutenbergClean gc = new GutenbergClean();
+		gc.processArguments(args);
+		gc.run();
+	}
+
+	private void run() throws IOException {
+		cleanText(this.getInputStream(), this.getOutputStream());
+	}
+
+	public void cleanText(InputStream is, OutputStream os) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		BufferedWriter bos = new BufferedWriter(new OutputStreamWriter(os));
+		String line;
+		boolean content = false;
+		while ((line = br.readLine()) != null) {
+			if (content) {
+				if (line.startsWith(PG_MARK_END)) {
+					content = false;
+				} else {
+					bos.write(line);
+					bos.write(lineSeparator);
+				}
+			} else if (line.startsWith(PG_MARK_START)) {
+				content = true;
+			}
+		}
+		bos.flush();
+		bos.close();
+	}
+
+}
