c0f9367369678270cf79e5a01dce397c8576b295
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2014-12-12 11:48:02 +0100

    Added a class to download and clean Gutenberg files from zwergdrossel

---
+++ de.nilsreiter.pg/src/main/java/de/nilsreiter/pg/GutenbergCopy.java
@@ -0,0 +1,81 @@
+package de.nilsreiter.pg;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
+import org.kohsuke.args4j.Option;
+
+import de.nilsreiter.util.IOUtil;
+import de.uniheidelberg.cl.a10.Main;
+
+public class GutenbergCopy extends Main {
+
+	@Option(name = "--urllist", required = true)
+	File urlList;
+
+	@Option(name = "--output")
+	File outputDirectory = new File("target/test/data");
+
+	char remoteSeparator = '/';
+
+	String remoteRoot = "/home/users0/reiterns/local/gutenberg";
+	String remoteHost = "zd";
+
+	File tempdir;
+	GutenbergClean cleaner;
+
+	public static void main(String[] args) throws IOException,
+			InterruptedException {
+		GutenbergCopy gc = new GutenbergCopy();
+		gc.processArguments(args);
+		gc.run();
+	}
+
+	private void run() throws IOException, InterruptedException {
+		if (!outputDirectory.exists()) outputDirectory.mkdirs();
+		tempdir = IOUtil.createTempDir("GutenbergCopy", "");
+		cleaner = new GutenbergClean('\n');
+
+		BufferedReader br = new BufferedReader(new FileReader(urlList));
+		String line;
+		while ((line = br.readLine()) != null) {
+			URL url = new URL(line);
+			this.copyFromURL(url);
+		}
+		br.close();
+	}
+
+	protected void copyFromURL(URL url) throws IOException,
+			InterruptedException {
+		String localname = url.getFile().substring(8);
+		String remotePath = getRemotePath(localname);
+
+		File targetFile = new File(tempdir, localname);
+		Process proc =
+				Runtime.getRuntime().exec(
+						new String[] {
+								"scp",
+								remoteHost + ":" + remoteRoot + remoteSeparator
+										+ remotePath,
+								targetFile.getAbsolutePath() });
+		proc.waitFor();
+		cleaner.cleanText(new FileInputStream(targetFile),
+				new FileOutputStream(new File(outputDirectory, localname
+						+ ".txt")));
+	}
+
+	public String getRemotePath(String name) {
+		StringBuilder b = new StringBuilder();
+		char[] nameArr = name.toCharArray();
+		for (int i = 0; i < nameArr.length - 1; i++) {
+			b.append(nameArr[i]).append(remoteSeparator);
+		}
+		b.append(name).append(remoteSeparator).append(name).append(".txt");
+		return b.toString();
+	}
+}
