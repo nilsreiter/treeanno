81b9e9eb7407011fae428ee15d7e46c04f4f988b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-01-15 09:52:00 +0100

    Now we can control if we want annototations and what annotations

--- de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/infinitejest/InfiniteJestCollectionReader.java
+++ de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/infinitejest/InfiniteJestCollectionReader.java
@@ -2,21 +2,45 @@ package de.nilsreiter.pipeline.segmentation.infinitejest;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.TypeCapability;
+import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.factory.JCasBuilder;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.util.Progress;
 
 import de.nilsreiter.pipeline.segmentation.type.Segment;
+import de.nilsreiter.pipeline.segmentation.type.SegmentBoundary;
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
 
+@TypeCapability(
+		outputs = { "de.nilsreiter.pipeline.segmentation.type.Segment" })
 public class InfiniteJestCollectionReader extends
 		JCasResourceCollectionReader_ImplBase {
 
+	public static final String PARAM_CREATE_BOUNDARY_ANNOTATION =
+			"Create Boundary Annotation";
+	public static final String PARAM_CREATE_SEGMENT_ANNOTATION =
+			"Create Segment Annotation";
+
 	boolean next = true;
+
+	@ConfigurationParameter(name = PARAM_CREATE_BOUNDARY_ANNOTATION,
+			defaultValue = "true")
+	boolean createBoundaries = true;
+
+	@ConfigurationParameter(name = PARAM_CREATE_SEGMENT_ANNOTATION,
+			defaultValue = "true")
+	boolean createSegments = true;
+
+	List<String> restriction = Arrays.asList("AFR", "EHDRH", "ETA");
 
 	@Override
 	public boolean hasNext() throws IOException, CollectionException {
@@ -57,8 +81,10 @@ public class InfiniteJestCollectionReader extends
 				if (tag.startsWith("/")) {
 					String rtag = tag.substring(1);
 					if (openSegments.containsKey(rtag)) {
-						jb.add(openSegments.get(rtag), Segment.class).setValue(
-								rtag);
+						if (restriction.contains(rtag)
+								&& (createSegments || createBoundaries))
+							jb.add(openSegments.get(rtag), Segment.class)
+									.setValue(rtag);
 						openSegments.remove(rtag);
 					}
 				} else {
@@ -73,10 +99,14 @@ public class InfiniteJestCollectionReader extends
 		}
 
 		isr.close();
-
 		jb.close();
+
+		if (createBoundaries)
+			for (Segment segment : JCasUtil.select(jCas, Segment.class)) {
+				AnnotationFactory.createAnnotation(jCas, segment.getBegin(),
+						segment.getBegin() + 1, SegmentBoundary.class);
+			}
 
 		next = false;
 	}
-
 }
--- de.nilsreiter.pipeline.segmentation/src/test/java/de/nilsreiter/pipeline/segmentation/infinitejest/TestInfiniteJestCollectionReader.java
+++ de.nilsreiter.pipeline.segmentation/src/test/java/de/nilsreiter/pipeline/segmentation/infinitejest/TestInfiniteJestCollectionReader.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.JCasIterable;
 import org.apache.uima.fit.pipeline.SimplePipeline;
@@ -14,10 +19,13 @@ import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Test;
 
 import de.nilsreiter.pipeline.segmentation.type.Segment;
+import de.nilsreiter.pipeline.segmentation.type.SegmentBoundary;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 
 public class TestInfiniteJestCollectionReader {
+
 	@Test
-	public void testCollectionReader() throws ResourceInitializationException {
+	public void testText() throws ResourceInitializationException {
 		String filePath =
 				"/Users/reiterns/Documents/Java/de.nilsreiter.pipeline.segmentation/src/test/resources/infinite_jest_annotated.txt";
 
@@ -25,8 +33,12 @@ public class TestInfiniteJestCollectionReader {
 				CollectionReaderFactory
 						.createReaderDescription(
 								InfiniteJestCollectionReader.class,
-								InfiniteJestCollectionReader.PARAM_INPUT_FILE,
-								filePath);
+								InfiniteJestCollectionReader.PARAM_SOURCE_LOCATION,
+								filePath,
+								InfiniteJestCollectionReader.PARAM_CREATE_BOUNDARY_ANNOTATION,
+								false,
+						InfiniteJestCollectionReader.PARAM_CREATE_SEGMENT_ANNOTATION,
+						false);
 
 		JCasIterable jcasIter = SimplePipeline.iteratePipeline(cr);
 		JCas jcas = jcasIter.iterator().next();
@@ -37,17 +49,81 @@ public class TestInfiniteJestCollectionReader {
 				jcas.getDocumentText().substring(10000, 10010));
 		assertFalse(jcas.getDocumentText().contains("<"));
 		assertFalse(jcas.getDocumentText().contains(">"));
+		assertFalse(JCasUtil.exists(jcas, Segment.class));
+		assertFalse(JCasUtil.exists(jcas, SegmentBoundary.class));
+	}
+
+	@Test
+	public void testSegmentAnnotation() throws Exception {
+		String filePath =
+				"/Users/reiterns/Documents/Java/de.nilsreiter.pipeline.segmentation/src/test/resources/infinite_jest_annotated.txt";
+
+		CollectionReaderDescription cr =
+				CollectionReaderFactory
+						.createReaderDescription(
+								InfiniteJestCollectionReader.class,
+								InfiniteJestCollectionReader.PARAM_SOURCE_LOCATION,
+								filePath,
+								InfiniteJestCollectionReader.PARAM_CREATE_BOUNDARY_ANNOTATION,
+								false,
+								InfiniteJestCollectionReader.PARAM_CREATE_SEGMENT_ANNOTATION,
+								true);
+
+		JCasIterable jcasIter = SimplePipeline.iteratePipeline(cr);
+		JCas jcas = jcasIter.iterator().next();
 
 		assertTrue(JCasUtil.exists(jcas, Segment.class));
 		Segment segment;
 
 		segment = JCasUtil.selectByIndex(jcas, Segment.class, 0);
 		assertEquals(3262, segment.getBegin());
-		assertEquals(42807, segment.getEnd());
-		assertEquals("HAL", segment.getValue());
+		assertEquals("ETA", segment.getValue());
+		assertEquals(24147, segment.getEnd());
 
 		segment = JCasUtil.selectByIndex(jcas, Segment.class, 1);
-		assertEquals(3262, segment.getBegin());
-		assertEquals("INC", segment.getValue());
+		assertEquals(28207, segment.getBegin());
+		assertEquals("ETA", segment.getValue());
+	}
+
+	@Test
+	public void testConversion() throws Exception {
+		String filePath =
+				"/Users/reiterns/Documents/Java/de.nilsreiter.pipeline.segmentation/src/test/resources/*.txt";
+		String tFilePath =
+				"/Users/reiterns/Documents/Java/de.nilsreiter.pipeline.segmentation/src/test/resources/xmi/";
+		CollectionReader cr;
+		cr =
+				CollectionReaderFactory.createReader(
+						InfiniteJestCollectionReader.class,
+						InfiniteJestCollectionReader.PARAM_SOURCE_LOCATION,
+						filePath);
+		AnalysisEngine writer =
+				AnalysisEngineFactory.createEngine(XmiWriter.class,
+						XmiWriter.PARAM_TARGET_LOCATION,
+						new File(tFilePath).getAbsolutePath());
+		SimplePipeline.runPipeline(cr, writer);
+
+		assertTrue(true);
+
+	}
+
+	@Test
+	public void testBoundaryAnnotation() throws Exception {
+		String filePath =
+				"/Users/reiterns/Documents/Java/de.nilsreiter.pipeline.segmentation/src/test/resources/infinite_jest_annotated.txt";
+
+		CollectionReaderDescription cr =
+				CollectionReaderFactory
+						.createReaderDescription(
+								InfiniteJestCollectionReader.class,
+								InfiniteJestCollectionReader.PARAM_SOURCE_LOCATION,
+								filePath,
+								InfiniteJestCollectionReader.PARAM_CREATE_BOUNDARY_ANNOTATION,
+								true);
+
+		JCasIterable jcasIter = SimplePipeline.iteratePipeline(cr);
+		JCas jcas = jcasIter.iterator().next();
+		JCasUtil.exists(jcas, SegmentBoundary.class);
+
 	}
 }
