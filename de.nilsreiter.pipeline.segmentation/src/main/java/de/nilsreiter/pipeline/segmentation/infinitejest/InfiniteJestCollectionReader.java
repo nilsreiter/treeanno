64ee94f91640972fb1f338b50f69d4954f3c0946
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-01-15 09:22:55 +0100

    Fixed a bug that prevented the document to appear in the pipeline

--- de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/infinitejest/InfiniteJestCollectionReader.java
+++ de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/infinitejest/InfiniteJestCollectionReader.java
@@ -1,54 +1,32 @@
 package de.nilsreiter.pipeline.segmentation.infinitejest;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.uima.collection.CollectionException;
-import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.factory.JCasBuilder;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.util.Progress;
 
 import de.nilsreiter.pipeline.segmentation.type.Segment;
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
 
-public class InfiniteJestCollectionReader extends JCasCollectionReader_ImplBase {
+public class InfiniteJestCollectionReader extends
+		JCasResourceCollectionReader_ImplBase {
 
-	public static final String PARAM_INPUT_FILE = "Input File";
+	boolean next = true;
 
-	@ConfigurationParameter(name = PARAM_INPUT_FILE, mandatory = true)
-	String filePath = null;
-
+	@Override
 	public boolean hasNext() throws IOException, CollectionException {
-		return false;
+		return next;
 	}
 
+	@Override
 	public Progress[] getProgress() {
 
-		return new Progress[] { new Progress() {
-
-			private static final long serialVersionUID = 1L;
-
-			public long getCompleted() {
-				return 1;
-			}
-
-			public long getTotal() {
-				return 1;
-			}
-
-			public String getUnit() {
-				return "Document";
-			}
-
-			public boolean isApproximate() {
-				return false;
-			}
-		} };
+		return new Progress[] {};
 	}
 
 	enum Mode {
@@ -57,12 +35,14 @@ public class InfiniteJestCollectionReader extends JCasCollectionReader_ImplBase 
 
 	@Override
 	public void getNext(JCas jCas) throws IOException, CollectionException {
-		File file = new File(filePath);
+		Resource res = nextFile();
+		initCas(jCas, res);
 
-		InputStreamReader isr =
-				new InputStreamReader(new FileInputStream(file), "UTF-8");
+		// System.err.println("!");
+		InputStreamReader isr = new InputStreamReader(res.getInputStream());
 
 		Map<String, Integer> openSegments = new HashMap<String, Integer>();
+
 		JCasBuilder jb = new JCasBuilder(jCas);
 		StringBuilder sb = new StringBuilder();
 
@@ -93,21 +73,10 @@ public class InfiniteJestCollectionReader extends JCasCollectionReader_ImplBase 
 		}
 
 		isr.close();
-		/*
-		 * String fileContents = FileUtils.readFileToString(file, "UTF-8");
-		 * while (index < fileContents.length() && start >= 0 && end >= 0) {
-		 * start = fileContents.indexOf('<', index); end =
-		 * fileContents.indexOf('>', start); if (start >= 0 && end >= 0) {
-		 * String seg = fileContents.substring(start, end + 1);
-		 * jb.add(fileContents.substring(index, start - 1)); if
-		 * (seg.startsWith("</")) { String segValue = seg.substring(2,
-		 * seg.length() - 1); if (openSegments.containsKey(segValue)) {
-		 * jb.add(openSegments.get(segValue), Segment.class)
-		 * .setValue(segValue); openSegments.remove(segValue); } } else {
-		 * openSegments.put(seg.substring(1, seg.length() - 1),
-		 * jb.getPosition()); } index = end; } }
-		 */
 
 		jb.close();
+
+		next = false;
 	}
+
 }
