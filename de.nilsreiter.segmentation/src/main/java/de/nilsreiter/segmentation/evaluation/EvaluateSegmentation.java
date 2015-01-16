7c0a0293d556d6c7568d3b81357ca0a13ead8748
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-01-16 11:42:53 +0100

    Added main class and Option bean

---
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/evaluation/EvaluateSegmentation.java
@@ -0,0 +1,49 @@
+package de.nilsreiter.segmentation.evaluation;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+
+import de.nilsreiter.pipeline.segmentation.type.SegmentBoundary;
+import de.uniheidelberg.cl.a10.Main;
+
+public class EvaluateSegmentation extends Main {
+
+	EvaluationOptions options = new EvaluationOptions();
+
+	public static void main(String[] args) throws UIMAException, IOException {
+		EvaluateSegmentation es = new EvaluateSegmentation();
+		es.processArguments(args, es.options);
+		es.run();
+	}
+
+	public void run() throws UIMAException, IOException {
+		TypeSystemDescription tsd =
+				TypeSystemDescriptionFactory.createTypeSystemDescription();
+		JCas silverJCas =
+				JCasFactory.createJCas(options.getSilverFile()
+						.getAbsolutePath(), tsd);
+		JCas goldJCas =
+				JCasFactory.createJCas(options.getGoldFile().getAbsolutePath(),
+						tsd);
+
+		Class<? extends Annotation> annoType = SegmentBoundary.class;
+		Metric metric = null;
+		switch (options.getMetric()) {
+		case BreakDiff:
+			metric = MetricFactory.getMetric(BreakDifference.class, annoType);
+			break;
+		default:
+			metric = MetricFactory.getMetric(WindowDifference.class, annoType);
+		}
+		double score = Double.NaN;
+		if (metric.init(goldJCas)) score = metric.score(goldJCas, silverJCas);
+		System.out.println(score);
+
+	}
+}
