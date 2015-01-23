f8b18fdcd3c59668f67219f6444d1055229a92a2
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-01-23 12:35:05 +0100

    Now parameters are settable

--- de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/morphadorner/C99Annotator.java
+++ de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/morphadorner/C99Annotator.java
@@ -3,6 +3,7 @@ package de.nilsreiter.pipeline.segmentation.morphadorner;
 import java.util.List;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.TypeCapability;
 import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.jcas.JCas;
@@ -14,17 +15,27 @@ import edu.northwestern.at.morphadorner.corpuslinguistics.textsegmenter.C99TextS
 
 @TypeCapability(
 		inputs = { "de.nilsreiter.pipeline.segmentation.type.SegmentationUnit",
-				"de.nilsreiter.pipeline.segmentation.type.SegmentationSubUnit" },
+		"de.nilsreiter.pipeline.segmentation.type.SegmentationSubUnit" },
 		outputs = { "de.nilsreiter.pipeline.segmentation.type.SegmentBoundary" })
 public class C99Annotator extends MASegmenter {
+
+	public static final String PARAM_MASK_SIZE = "Mask Size";
+	public static final String PARAM_SEGMENTS_WANTED = "Segments Wanted";
+
+	@ConfigurationParameter(name = PARAM_MASK_SIZE, mandatory = false,
+			defaultValue = "11")
+	int maskSize = 11;
+	@ConfigurationParameter(name = PARAM_SEGMENTS_WANTED, mandatory = false,
+			defaultValue = "-1")
+	int segmentsWanted = -1;
 
 	@Override
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
 		this.createTokenLists(aJCas);
 
 		C99TextSegmenter c99 = new C99TextSegmenter();
-		c99.setSegmentsWanted(10);
-		c99.setMaskSize(5);
+		c99.setSegmentsWanted(segmentsWanted);
+		c99.setMaskSize(maskSize);
 		c99.setStopWords(new BaseStopWords());
 		c99.setStemmer(new PorterStemmer());
 
