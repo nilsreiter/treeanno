9aa73bae5a434b9b7cb691ae4caa81b7e371c708
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-02-17 14:24:08 +0100

    Added method to retrieve multiple auxiliaries

--- de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/clauselevel/ClauseUtil.java
+++ de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/clauselevel/ClauseUtil.java
@@ -1,5 +1,8 @@
 package de.nilsreiter.pipeline.segmentation.clauselevel;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import de.nilsreiter.pipeline.segmentation.clauselevel.type.DepRel;
 
 public class ClauseUtil {
@@ -11,5 +14,14 @@ public class ClauseUtil {
 				return dr.getDependents(i);
 		}
 		return null;
+	}
+
+	public static List<DepRel> getDependents(DepRel dr, String reln) {
+		List<DepRel> deps = new LinkedList<DepRel>();
+		for (int i = 0; i < dr.getDependents().size(); i++) {
+			if (dr.getDependents(i).getRelation().matches(reln))
+				deps.add(dr.getDependents(i));
+		}
+		return deps;
 	}
 }
