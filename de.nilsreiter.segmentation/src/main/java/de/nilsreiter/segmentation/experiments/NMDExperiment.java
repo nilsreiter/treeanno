031738cd4a47d54a9379e9bcabe5553e9dad35a8
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-02-17 14:25:33 +0100

    Finalised NMDExperiment class to replicate the Wallace (2012) stuff

--- de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/experiments/NMDExperiment.java
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/experiments/NMDExperiment.java
@@ -10,10 +10,17 @@ import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import de.nilsreiter.pipeline.segmentation.ParagraphAnnotator;
+import de.nilsreiter.pipeline.segmentation.Paragraphs2Files;
 import de.nilsreiter.pipeline.segmentation.Segment2Boundary;
 import de.nilsreiter.pipeline.segmentation.SegmentationSubUnitAnnotator;
 import de.nilsreiter.pipeline.segmentation.SegmentationUnitAnnotator;
+import de.nilsreiter.pipeline.segmentation.infinitejest.IJEvalPreparer;
+import de.nilsreiter.pipeline.segmentation.infinitejest.RelabelSegmentValues;
 import de.nilsreiter.pipeline.segmentation.topicmodeling.NarrativeDisentanglement;
+import de.nilsreiter.pipeline.segmentation.type.Segment;
+import de.nilsreiter.segmentation.evaluation.Metric;
+import de.nilsreiter.segmentation.evaluation.MetricFactory;
+import de.nilsreiter.segmentation.evaluation.PRF;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordLemmatizer;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
@@ -29,14 +36,18 @@ public class NMDExperiment extends Experiment {
 	protected AnalysisEngine[] getSegmentation()
 			throws ResourceInitializationException {
 		return new AnalysisEngine[] {
+				createEngine(Paragraphs2Files.class,
+						Paragraphs2Files.PARAM_OUTPUT_DIRECTORY,
+						new File(getWorkingDirectory(), "paragraphs")
+								.getAbsolutePath()),
 				createEngine(SegmentationUnitAnnotator.class,
 						SegmentationUnitAnnotator.PARAM_BASE_TYPE,
 						"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Paragraph"),
 						createEngine(SegmentationSubUnitAnnotator.class,
 								SegmentationSubUnitAnnotator.PARAM_BASE_TYPE,
 								"de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity"),
-				createEngine(NarrativeDisentanglement.class),
-				// createEngine(SegmentMerger.class),
+								createEngine(NarrativeDisentanglement.class),
+								// createEngine(SegmentMerger.class),
 								createEngine(Segment2Boundary.class) };
 	}
 
@@ -55,11 +66,30 @@ public class NMDExperiment extends Experiment {
 		Experiment tte =
 				new NMDExperiment(new File(
 						"/Users/reiterns/Documents/SegNarr/Experiments/"));
-		tte.setInputDirectoryName("/Users/reiterns/Documents/Workspace/Segmentation Corpus/corpus/infinite_jest_annotated.txt.xmi");
+		tte.setInputDirectoryName("/Users/reiterns/Documents/Workspace/Segmentation Corpus/corpus/en/infinite_jest_annotated.txt.xmi");
 
 		tte.doInitialization = false;
 		tte.doProcessing = false;
+		tte.doSegmentation = false;
+		tte.doGoldChain = true;
 		System.exit(tte.run());
+	}
+
+	@Override
+	protected AnalysisEngine[] getGoldChain()
+			throws ResourceInitializationException {
+		return new AnalysisEngine[] { createEngine(ParagraphAnnotator.class),
+				createEngine(IJEvalPreparer.class),
+				createEngine(RelabelSegmentValues.class) };
+
+	}
+
+	@Override
+	public Metric[] getMetrics() {
+		PRF m = MetricFactory.getMetric(PRF.class, Segment.class);
+		m.setFeatureName("Value");
+		m.setClassWise(false);
+		return new Metric[] { m };
 	}
 
 	@Override
