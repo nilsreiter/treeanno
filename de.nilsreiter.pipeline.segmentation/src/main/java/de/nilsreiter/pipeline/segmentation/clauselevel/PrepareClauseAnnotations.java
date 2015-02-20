6c8fed1948431954d75fe346d3bbd09d71b7b1e3
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-02-20 08:24:55 +0100

    Added code to fill the extent array

--- de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/clauselevel/PrepareClauseAnnotations.java
+++ de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/clauselevel/PrepareClauseAnnotations.java
@@ -1,13 +1,18 @@
 package de.nilsreiter.pipeline.segmentation.clauselevel;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
 
 import de.nilsreiter.pipeline.segmentation.clauselevel.type.Clause;
 import de.nilsreiter.pipeline.segmentation.clauselevel.type.DepRel;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 public class PrepareClauseAnnotations extends JCasAnnotator_ImplBase {
 	String patternDependencyType = "^(root|ccomp|advcl)$";
@@ -16,10 +21,32 @@ public class PrepareClauseAnnotations extends JCasAnnotator_ImplBase {
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
 		for (DepRel dr : JCasUtil.select(aJCas, DepRel.class)) {
 			if (dr.getRelation().matches(patternDependencyType)) {
-				AnnotationFactory.createAnnotation(aJCas, dr.getBegin(),
-						dr.getEnd(), Clause.class);
-			}
+				Clause clause =
+						AnnotationFactory.createAnnotation(aJCas,
+								dr.getBegin(), dr.getEnd(), Clause.class);
 
+				List<Token> extentList = getDependentTokens(dr);
+				FSArray arr = new FSArray(aJCas, extentList.size());
+				for (int i = 0; i < extentList.size(); i++) {
+					arr.set(i, extentList.get(i));
+				}
+				arr.addToIndexes();
+				clause.setExtent(arr);
+				clause.setHead(dr);
+			}
 		}
+	}
+
+	List<Token> getDependentTokens(DepRel dr) {
+		List<Token> tokList = new LinkedList<Token>();
+		if (dr.getDependents() != null)
+			for (int i = 0; i < dr.getDependents().size(); i++) {
+				DepRel dtr = dr.getDependents(i);
+				if (!dtr.getRelation().matches(patternDependencyType)) {
+					tokList.add(dtr.getToken());
+					tokList.addAll(getDependentTokens(dtr));
+				}
+			}
+		return tokList;
 	}
 }
