696826e14870603396106cf6a424ca5a2424054a
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-03-04 11:49:38 +0100

    Main class for converting plain text to CSV files for CrowdFlower

---
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/main/MakeCrowdData.java
@@ -0,0 +1,38 @@
+package de.nilsreiter.segmentation.main;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import de.nilsreiter.pipeline.segmentation.CSVExport;
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+
+public class MakeCrowdData {
+
+	static File inputDirectory = new File(
+			"/Users/reiterns/Documents/SegNarr/CrowdFlower/exp1/txt");
+	static File outputDirectory = new File(
+			"/Users/reiterns/Documents/SegNarr/CrowdFlower/exp1/csv");
+
+	public static void main(String[] args)
+			throws ResourceInitializationException, UIMAException, IOException {
+		SimplePipeline.runPipeline(CollectionReaderFactory
+				.createReaderDescription(TextReader.class,
+						TextReader.PARAM_LANGUAGE, "en",
+						TextReader.PARAM_SOURCE_LOCATION,
+						inputDirectory.getAbsolutePath() + File.separatorChar
+								+ "*.txt"), AnalysisEngineFactory
+				.createEngineDescription(StanfordSegmenter.class),
+						AnalysisEngineFactory.createEngineDescription(CSVExport.class,
+						CSVExport.PARAM_OUTPUT_DIRECTORY,
+						outputDirectory.getAbsolutePath(),
+								CSVExport.PARAM_PREFIX_SIZE, 10));
+	}
+
+}
