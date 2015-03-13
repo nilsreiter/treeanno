7782dfc017ffbb7ca3d0bc8e62233ea7ff81d194
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-03-13 07:45:11 +0100

    Now Candidate annotator is configurable

--- de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/BoundaryCandidateAnnotator.java
+++ de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/BoundaryCandidateAnnotator.java
@@ -2,20 +2,39 @@ package de.nilsreiter.pipeline.segmentation;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 
 import de.nilsreiter.pipeline.segmentation.type.SegmentBoundaryCandidate;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 
 public class BoundaryCandidateAnnotator extends JCasAnnotator_ImplBase {
 
+	public final static String PARAM_BASE_ANNOTATION = "Base Annotation";
+
+	@ConfigurationParameter(
+			name = PARAM_BASE_ANNOTATION,
+			mandatory = false,
+			defaultValue = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence")
+	String baseAnnotationType =
+			"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence";
+
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-		for (Sentence sentence : JCasUtil.select(jcas, Sentence.class)) {
-			AnnotationFactory.createAnnotation(jcas, sentence.getBegin(),
-					sentence.getEnd(), SegmentBoundaryCandidate.class);
+
+		try {
+			Class<? extends Annotation> annoClass =
+					(Class<? extends Annotation>) Class
+							.forName(baseAnnotationType);
+			for (Annotation sentence : JCasUtil.select(jcas, annoClass)) {
+				AnnotationFactory.createAnnotation(jcas, sentence.getBegin(),
+						sentence.getEnd(), SegmentBoundaryCandidate.class);
+			}
+		} catch (ClassNotFoundException e) {
+			throw new AnalysisEngineProcessException(e);
 		}
+
 	}
 }
