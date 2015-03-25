1a12879e518843eb491094fbbf8de81652de046c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-03-25 16:54:26 +0100

    Changes for the Annotate-Athon

--- de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/main/MakeCrowdData.java
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/main/MakeCrowdData.java
@@ -10,25 +10,26 @@ import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import de.nilsreiter.pipeline.segmentation.CSVExport;
-import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
 
 public class MakeCrowdData {
 
 	static File inputDirectory = new File(
-			"/Users/reiterns/Documents/SegNarr/CrowdFlower/exp1/txt");
+			"/Users/reiterns/Documents/SegNarr/CrowdFlower/exp2/xmi-in");
 	static File outputDirectory = new File(
-			"/Users/reiterns/Documents/SegNarr/CrowdFlower/exp1/csv");
+			"/Users/reiterns/Documents/SegNarr/CrowdFlower/exp2/csv");
 
 	// TODO: Prepare an XMI file that can be imported easily
 	public static void main(String[] args)
 			throws ResourceInitializationException, UIMAException, IOException {
 		SimplePipeline.runPipeline(CollectionReaderFactory
-				.createReaderDescription(TextReader.class,
-						TextReader.PARAM_LANGUAGE, "en",
-						TextReader.PARAM_SOURCE_LOCATION,
+				.createReaderDescription(XmiReader.class,
+						XmiReader.PARAM_LANGUAGE, "en",
+						XmiReader.PARAM_SOURCE_LOCATION,
 						inputDirectory.getAbsolutePath() + File.separatorChar
-						+ "*.txt"), AnalysisEngineFactory
+						+ "*.xmi", XmiReader.PARAM_LENIENT, true),
+						AnalysisEngineFactory
 						.createEngineDescription(StanfordSegmenter.class),
 				AnalysisEngineFactory.createEngineDescription(CSVExport.class,
 								CSVExport.PARAM_OUTPUT_DIRECTORY,
