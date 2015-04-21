d6a30f5c34073d2358bb729df7420f4a3ca8b809
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-21 11:50:09 +0200

    added main class to convert v2 annotations

---
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/main/AnnoV2ToXMI.java
@@ -0,0 +1,51 @@
+package de.nilsreiter.segmentation.main;
+
+import java.io.File;
+
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+
+import com.lexicalscope.jewel.cli.CliFactory;
+import com.lexicalscope.jewel.cli.Option;
+
+import de.nilsreiter.pipeline.io.TextReader;
+import de.nilsreiter.pipeline.segmentation.annotation.AnnotationReaderV2;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+
+public class AnnoV2ToXMI {
+
+	public static void main(String[] args) throws Exception {
+		Options options = CliFactory.parseArguments(Options.class, args);
+
+		CollectionReaderDescription crd =
+				CollectionReaderFactory.createReaderDescription(
+						TextReader.class, TextReader.PARAM_SOURCE_LOCATION,
+						options.getRawDirectory() + "/*.txt");
+		SimplePipeline.runPipeline(crd, AnalysisEngineFactory
+				.createEngineDescription(AnnotationReaderV2.class,
+						AnnotationReaderV2.PARAM_DIRECTORY_NAME, options
+								.getAnnoDirectory().getAbsolutePath(),
+						AnnotationReaderV2.PARAM_FILE_SUFFIX, options
+								.getFileSuffix()), AnalysisEngineFactory
+				.createEngineDescription(XmiWriter.class,
+						XmiWriter.PARAM_TARGET_LOCATION,
+						options.getOutputDirectory()));
+	}
+
+	public interface Options {
+		@Option
+		File getRawDirectory();
+
+		@Option
+		File getAnnoDirectory();
+
+		@Option
+		File getOutputDirectory();
+
+		@Option
+		String getFileSuffix();
+	}
+
+}
