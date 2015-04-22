9753a285c5142ebafa8fbb41193a414b210cb890
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-22 09:46:54 +0200

    Now we can set the potential boundary type using the interface

--- de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/evaluation/impl/BoundarySimilarity_impl.java
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/evaluation/impl/BoundarySimilarity_impl.java
@@ -8,11 +8,12 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.python.core.PyTuple;
 
-import de.nilsreiter.segmentation.evaluation.SegmentationSimilarity;
+import de.nilsreiter.segmentation.evaluation.BoundarySimilarity;
 
 public class BoundarySimilarity_impl extends AbstractSegEvalMetric implements
-SegmentationSimilarity {
+		BoundarySimilarity {
 	Class<? extends Annotation> annoType;
+	Class<? extends Annotation> potentialBoundaryType = null;
 
 	public BoundarySimilarity_impl(Class<? extends Annotation> annotationType) {
 		annoType = annotationType;
@@ -25,19 +26,33 @@ SegmentationSimilarity {
 
 	public Map<String, Double> score(JCas gold, JCas silver) {
 		ensureInterpreter();
-
-		PyTuple goldTuple =
-				getMassTuple(JCasUtil.select(gold, annoType), gold
-						.getDocumentText().length());
-		PyTuple silverTuple =
-				getMassTuple(JCasUtil.select(silver, annoType), silver
-						.getDocumentText().length());
+		PyTuple goldTuple, silverTuple;
+		if (potentialBoundaryType == null) {
+			goldTuple =
+					getMassTuple(JCasUtil.select(gold, annoType), gold
+							.getDocumentText().length());
+			silverTuple =
+					getMassTuple(JCasUtil.select(silver, annoType), silver
+							.getDocumentText().length());
+		} else {
+			goldTuple = getMassTuple(gold, annoType, potentialBoundaryType);
+			silverTuple = getMassTuple(silver, annoType, potentialBoundaryType);
+		}
 
 		Map<String, Double> r = new HashMap<String, Double>();
 		r.put(getClass().getSimpleName(),
 				getPyFunctionValue(goldTuple, silverTuple,
 						"segeval.boundary_similarity"));
 		return r;
+	}
+
+	public Class<? extends Annotation> getPotentialBoundaryType() {
+		return potentialBoundaryType;
+	}
+
+	public void setPotentialBoundaryType(
+			Class<? extends Annotation> potentialBoundaryType) {
+		this.potentialBoundaryType = potentialBoundaryType;
 	}
 
 }
