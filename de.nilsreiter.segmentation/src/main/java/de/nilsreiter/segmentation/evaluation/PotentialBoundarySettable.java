0230dfe6e64eefc666cd74d797d2ab2205402bae
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-23 15:59:57 +0200

    PotentialBoundarySettable is now deprecated

--- de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/evaluation/PotentialBoundarySettable.java
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/evaluation/PotentialBoundarySettable.java
@@ -2,11 +2,14 @@ package de.nilsreiter.segmentation.evaluation;
 
 import org.apache.uima.jcas.tcas.Annotation;
 
+@Deprecated
 public interface PotentialBoundarySettable {
 
+	@Deprecated
 	void setPotentialBoundaryType(
 			Class<? extends Annotation> potentialBoundaryType);
 
+	@Deprecated
 	Class<? extends Annotation> getPotentialBoundaryType();
 
 }
