39c601b48b2cdb11320e002892d56432b3dcc02d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-23 09:37:07 +0200

    Now we can set the language on the command line and run a segmenter on the text

--- de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/main/AnnoV2ToXMI.java
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/main/AnnoV2ToXMI.java
@@ -1,7 +1,10 @@
 package de.nilsreiter.segmentation.main;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
@@ -10,9 +13,11 @@ import org.apache.uima.fit.pipeline.SimplePipeline;
 import com.lexicalscope.jewel.cli.CliFactory;
 import com.lexicalscope.jewel.cli.Option;
 
+import de.nilsreiter.pipeline.PipelineBuilder;
 import de.nilsreiter.pipeline.io.TextReader;
 import de.nilsreiter.pipeline.segmentation.annotation.AnnotationReaderV2;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.tudarmstadt.ukp.dkpro.core.languagetool.LanguageToolSegmenter;
 
 public class AnnoV2ToXMI {
 
@@ -22,16 +27,24 @@ public class AnnoV2ToXMI {
 		CollectionReaderDescription crd =
 				CollectionReaderFactory.createReaderDescription(
 						TextReader.class, TextReader.PARAM_SOURCE_LOCATION,
-						options.getRawDirectory() + "/*.txt");
-		SimplePipeline.runPipeline(crd, AnalysisEngineFactory
-				.createEngineDescription(AnnotationReaderV2.class,
-						AnnotationReaderV2.PARAM_DIRECTORY_NAME, options
-								.getAnnoDirectory().getAbsolutePath(),
-						AnnotationReaderV2.PARAM_FILE_SUFFIX, options
-								.getFileSuffix()), AnalysisEngineFactory
-				.createEngineDescription(XmiWriter.class,
-						XmiWriter.PARAM_TARGET_LOCATION,
-						options.getOutputDirectory()));
+						options.getRawDirectory() + "/*.txt",
+						TextReader.PARAM_LANGUAGE, options.getLanguage());
+
+		List<AnalysisEngineDescription> pl =
+				new LinkedList<AnalysisEngineDescription>();
+		pl.add(AnalysisEngineFactory.createEngineDescription(
+				AnnotationReaderV2.class,
+				AnnotationReaderV2.PARAM_DIRECTORY_NAME, options
+				.getAnnoDirectory().getAbsolutePath(),
+				AnnotationReaderV2.PARAM_FILE_SUFFIX, options.getFileSuffix()));
+		if (options.getAddTokens()) {
+			pl.add(AnalysisEngineFactory
+					.createEngineDescription(LanguageToolSegmenter.class));
+		}
+		pl.add(AnalysisEngineFactory.createEngineDescription(XmiWriter.class,
+				XmiWriter.PARAM_TARGET_LOCATION, options.getOutputDirectory()));
+
+		SimplePipeline.runPipeline(crd, PipelineBuilder.array(pl));
 	}
 
 	public interface Options {
@@ -46,6 +59,12 @@ public class AnnoV2ToXMI {
 
 		@Option
 		String getFileSuffix();
+
+		@Option
+		boolean getAddTokens();
+
+		@Option
+		String getLanguage();
 	}
 
 }
