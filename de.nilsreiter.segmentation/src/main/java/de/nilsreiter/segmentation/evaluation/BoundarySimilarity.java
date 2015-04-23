1929205c9defe3faa75665792672e5917a3fe1f1
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-23 17:41:26 +0200

    removed methods on potential boundaries

--- de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/evaluation/BoundarySimilarity.java
+++ de.nilsreiter.segmentation/src/main/java/de/nilsreiter/segmentation/evaluation/BoundarySimilarity.java
@@ -1,12 +1,6 @@
 package de.nilsreiter.segmentation.evaluation;
 
-import org.apache.uima.jcas.tcas.Annotation;
 
 public interface BoundarySimilarity extends Metric {
-
-	void setPotentialBoundaryType(
-			Class<? extends Annotation> potentialBoundaryType);
-
-	Class<? extends Annotation> getPotentialBoundaryType();
 
 }
