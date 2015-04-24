cf729f054eac08948659c478e4c1e5f1cbd014bd
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-24 11:05:01 +0200

    fix: better annotation file repairing

--- de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/annotation/AnnotationReaderV2.java
+++ de.nilsreiter.pipeline.segmentation/src/main/java/de/nilsreiter/pipeline/segmentation/annotation/AnnotationReaderV2.java
@@ -49,36 +49,55 @@ public class AnnotationReaderV2 extends JCasAnnotator_ImplBase {
 			// throw new AnalysisEngineProcessException();
 		}
 
+		contents = contents.replaceAll("<b1>\\s*<b2>", "<b1>");
+		contents = contents.replaceAll("<b2>\\s*<b1>", "<b1>");
+		contents = contents.replaceAll("<b1>\\s*<b3>", "<b1>");
+		contents = contents.replaceAll("<b3>\\s*<b1>", "<b1>");
+
+		contents = contents.replaceAll("<b3>\\s*<b2>", "<b2>");
+		contents = contents.replaceAll("<b2>\\s*<b3>", "<b2>");
+
+		contents = contents.replaceAll("<b1>\\s*<b1>", "<b1>");
+		contents = contents.replaceAll("<b2>\\s*<b2>", "<b2>");
+		contents = contents.replaceAll("<b3>\\s*<b3>", "<b3>");
+
+		contents = contents.replaceAll("<b4>", "");
+
 		int pos = 0;
 		int ind;
-		int l = 15;
+		int l = 10;
 		String text = jcas.getDocumentText();
+		int textPost = 0;
 		do {
 			ind = contents.indexOf("<b", pos);
 			char lev;
 			if (ind > 0) {
 				lev = contents.charAt(ind + 2);
 				String post = contents.substring(ind + 4, ind + l).trim();
-				int textPost = text.indexOf(post);
+				textPost = text.indexOf(post, textPost);
 				int level = Character.getNumericValue(lev);
-				switch (lev) {
-				case '1':
-					AnnotationFactory.createAnnotation(jcas, textPost,
-							textPost + 1, SegmentBoundaryLevel1.class)
-							.setLevel(level);
-					break;
-				case '2':
-					AnnotationFactory.createAnnotation(jcas, textPost,
-							textPost + 1, SegmentBoundaryLevel2.class)
-							.setLevel(level);
-					break;
-				case '3':
-					AnnotationFactory.createAnnotation(jcas, textPost,
-							textPost + 1, SegmentBoundaryLevel3.class)
-							.setLevel(level);
-					break;
+				if (textPost > 0)
+					switch (lev) {
+					case '1':
+						AnnotationFactory.createAnnotation(jcas, textPost,
+								textPost, SegmentBoundaryLevel1.class)
+								.setLevel(level);
+						break;
+					case '2':
+						AnnotationFactory.createAnnotation(jcas, textPost,
+								textPost, SegmentBoundaryLevel2.class)
+								.setLevel(level);
+						break;
+					case '3':
+						AnnotationFactory.createAnnotation(jcas, textPost,
+								textPost, SegmentBoundaryLevel3.class)
+								.setLevel(level);
+						break;
+					}
+				else {
+					System.err.println(file.getName() + ": text not found: "
+							+ post);
 				}
-
 				pos = ind + 1;
 			}
 		} while (ind > 0);
