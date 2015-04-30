c998e3554d6c7df98f5a45983391449ea66c6435
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-30 19:45:03 +0200

    first unit test for annotation cleaner

---
+++ de.nilsreiter.pipeline.segmentation/src/test/java/de/nilsreiter/pipeline/segmentation/annotation/TestAnnotationCleanerV2.java
@@ -0,0 +1,49 @@
+package de.nilsreiter.pipeline.segmentation.annotation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.AnnotationFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.nilsreiter.pipeline.segmentation.type.SegmentBoundaryLevel3;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+
+public class TestAnnotationCleanerV2 {
+	JCas jcas;
+
+	@Before
+	public void setUp() throws UIMAException {
+		jcas = JCasFactory.createJCas();
+		jcas.setDocumentText("The dog barks, because he is hungry.");
+		jcas.setDocumentLanguage("en");
+		SimplePipeline.runPipeline(jcas, AnalysisEngineFactory
+				.createEngineDescription(StanfordSegmenter.class));
+
+	}
+
+	@Test
+	public void testAnnotationCleaner() throws AnalysisEngineProcessException,
+			ResourceInitializationException {
+		AnnotationFactory.createAnnotation(jcas, 4, 7,
+				SegmentBoundaryLevel3.class);
+		AnnotationFactory.createAnnotation(jcas, 4, 7,
+				SegmentBoundaryLevel3.class);
+
+		SimplePipeline.runPipeline(jcas, AnalysisEngineFactory
+				.createEngineDescription(AnnotationCleanerV2.class));
+
+		assertTrue(JCasUtil.exists(jcas, SegmentBoundaryLevel3.class));
+		assertEquals(1, JCasUtil.select(jcas, SegmentBoundaryLevel3.class));
+
+	}
+}
