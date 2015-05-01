5a7c7bde95d2971c52584f12f9832c350e06a1b2
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-05-01 11:30:37 +0200

    Added SegmentBoundaryAnnotator as a helper utiliy

---
+++ de.ustu.creta.segmentation.evaluation/segmentation.evaluation/src/main/java/de/ustu/creta/segmentation/evaluation/util/SegmentBoundaryAnnotator.java
@@ -0,0 +1,37 @@
+package de.ustu.creta.segmentation.evaluation.util;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.factory.AnnotationFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+
+import de.nilsreiter.pipeline.segmentation.type.SegmentBoundary;
+
+public class SegmentBoundaryAnnotator extends JCasAnnotator_ImplBase {
+
+	public static final String PARAM_ANNOTATION_TYPE = "Annotation Type";
+
+	@ConfigurationParameter(name = PARAM_ANNOTATION_TYPE)
+	String annotationTypeName = "";
+	Class<? extends Annotation> annotationType;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		try {
+			Class<?> clazz = Class.forName(annotationTypeName);
+			annotationType = (Class<? extends Annotation>) clazz;
+		} catch (ClassNotFoundException e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+		for (Annotation anno : JCasUtil.select(jcas, annotationType)) {
+			int b = anno.getBegin();
+			AnnotationFactory.createAnnotation(jcas, b, b,
+					SegmentBoundary.class);
+		}
+	}
+
+}
