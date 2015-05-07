6e386cd65c233c199ce1996d167e60c78713a9e0
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-05-07 18:04:39 +0200

    Asking for observed agreement now a method of Agreement

--- de.ustu.creta.segmentation.evaluation/segmentation.evaluation/src/main/java/de/ustu/creta/segmentation/agreement/Agreement.java
+++ de.ustu.creta.segmentation.evaluation/segmentation.evaluation/src/main/java/de/ustu/creta/segmentation/agreement/Agreement.java
@@ -2,6 +2,13 @@ package de.ustu.creta.segmentation.agreement;
 
 import org.apache.uima.jcas.JCas;
 
+import de.ustu.creta.segmentation.evaluation.Metric;
+
 public interface Agreement {
 	public double agr(JCas... jcas);
+
+	void setObservedAgreementMetric(Metric metric);
+
+	Metric getObservedAgreementMetric();
+
 }
--- de.ustu.creta.segmentation.evaluation/segmentation.evaluation/src/main/java/de/ustu/creta/segmentation/agreement/CohensKappa.java
+++ de.ustu.creta.segmentation.evaluation/segmentation.evaluation/src/main/java/de/ustu/creta/segmentation/agreement/CohensKappa.java
@@ -2,14 +2,10 @@ package de.ustu.creta.segmentation.agreement;
 
 import org.apache.uima.jcas.JCas;
 
-import de.ustu.creta.segmentation.evaluation.Metric;
-
 public interface CohensKappa extends Agreement {
-	void setObservedAgreementMetric(Metric metric);
 
 	double getObservedAgreement(JCas... jcas);
 
 	double getChanceAgreement(JCas... jcas);
 
-	Metric getObservedAgreementMetric();
 }
