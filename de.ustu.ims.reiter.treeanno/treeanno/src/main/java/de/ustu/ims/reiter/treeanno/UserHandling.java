32ca2db73fb74a8a1d42d8017331546f6be0b2d5
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-06-03 17:24:52 +0200

    new login screen, defunct

---
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/UserHandling.java
@@ -0,0 +1,25 @@
+package de.ustu.ims.reiter.treeanno;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserHandling
+ */
+public class UserHandling extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
