a3c1451beecfac4f6255780db8deb1d7e3c1d99c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-06-08 21:12:29 +0200

    New bean Project, Documents now know to which project they belong

---
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/beans/Project.java
@@ -0,0 +1,22 @@
+package de.ustu.ims.reiter.treeanno.beans;
+
+public class Project {
+	int databaseId;
+	String name;
+
+	public int getDatabaseId() {
+		return databaseId;
+	}
+
+	public void setDatabaseId(int databaseId) {
+		this.databaseId = databaseId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
