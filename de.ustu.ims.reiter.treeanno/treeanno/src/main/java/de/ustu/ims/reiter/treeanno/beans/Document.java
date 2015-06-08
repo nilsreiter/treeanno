a4f59eddc9d217feac397dd404e2bd54c2dddd87
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-06-08 15:24:57 +0200

    Bean for document meta data

---
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/beans/Document.java
@@ -0,0 +1,25 @@
+package de.ustu.ims.reiter.treeanno.beans;
+
+import java.util.Date;
+
+public class Document {
+	int id;
+	Date modificationDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(Date modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+
+}
