522639cbdfbadf1bfd087a70a25a952dc408f60a
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-06-24 16:35:22 +0200

    Added a wrapper around the servlet context to access context attributes in a type-safe manner

--- de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/CA.java
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/CA.java
@@ -2,4 +2,7 @@ package de.ustu.ims.reiter.treeanno;
 
 public class CA {
 	public static final String PROJECTLIST = "projectlist";
+	public static final String USER = "User";
+	public static final String DATABASEIO = "DatabaseIO";
+	public static final String DATALAYER = "DataLayer";
 }
---
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/CW.java
@@ -0,0 +1,24 @@
+package de.ustu.ims.reiter.treeanno;
+
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import de.ustu.ims.reiter.treeanno.beans.Project;
+import de.ustu.ims.reiter.treeanno.io.DatabaseIO;
+
+public class CW {
+	public static DatabaseIO getDatabaseIO(ServletContext sc) {
+		return (DatabaseIO) sc.getAttribute(CA.DATABASEIO);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static List<Project> getProjectList(ServletContext sc) {
+		return (List<Project>) sc.getAttribute(CA.PROJECTLIST);
+	}
+
+	public static DataLayer getDataLayer(ServletContext sc) {
+		return (DataLayer) sc.getAttribute(CA.DATALAYER);
+	}
+
+}
