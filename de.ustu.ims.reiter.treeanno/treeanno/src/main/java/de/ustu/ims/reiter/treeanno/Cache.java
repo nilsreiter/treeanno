652b938ee9b930a15cf12dc9fe6824f7e410b3a0
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-06-24 20:12:03 +0200

    fully implemented, but only delegates

--- de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/Cache.java
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/Cache.java
@@ -3,6 +3,8 @@ package de.ustu.ims.reiter.treeanno;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.uima.jcas.JCas;
+
 import de.ustu.ims.reiter.treeanno.beans.Document;
 import de.ustu.ims.reiter.treeanno.beans.Project;
 import de.ustu.ims.reiter.treeanno.beans.User;
@@ -44,5 +46,25 @@ public class Cache implements DataLayer {
 	@Override
 	public Collection<Document> getDocuments(int projectId) {
 		return dbio.getDocuments(projectId);
+	}
+
+	@Override
+	public JCas getJCas(Document document) {
+		return dbio.getJCas(document);
+	}
+
+	@Override
+	public boolean deleteDocument(Document document) {
+		return dbio.deleteDocument(document);
+	}
+
+	@Override
+	public int cloneDocument(Document document) {
+		return dbio.cloneDocument(document);
+	}
+
+	@Override
+	public boolean updateJCas(Document document, JCas jcas) {
+		return dbio.updateJCas(document, jcas);
 	}
 }
