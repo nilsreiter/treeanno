ba4808c9b2ec3efd0f77472d2079763d481708c8
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-07-13 08:59:06 +0200

    Moved mapping component to new class

--- de.ustu.ims.reiter.treeanno.tools/treeanno.tools/src/main/java/de/ustu/ims/reiter/treeanno/tools/ConvertDocuments.java
+++ de.ustu.ims.reiter.treeanno.tools/treeanno.tools/src/main/java/de/ustu/ims/reiter/treeanno/tools/ConvertDocuments.java
@@ -4,26 +4,16 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.uima.UIMAException;
-import org.apache.uima.UimaContext;
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.collection.CollectionReaderDescription;
-import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
-import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.fit.util.JCasUtil;
-import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
-import org.apache.uima.resource.ResourceInitializationException;
 
 import com.lexicalscope.jewel.cli.CliFactory;
 import com.lexicalscope.jewel.cli.Option;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
-import de.ustu.ims.reiter.treeanno.api.type.TreeSegment;
 
 public class ConvertDocuments {
 
@@ -54,46 +44,6 @@ public class ConvertDocuments {
 
 		@Option
 		String getSegmentClassName();
-	}
-
-	public static class MapToTreeAnnoClass extends JCasAnnotator_ImplBase {
-
-		public static final String PARAM_CLASSNAME = "Class name";
-
-		@ConfigurationParameter(name = PARAM_CLASSNAME)
-		String className;
-
-		Class<? extends Annotation> annoClass;
-
-		@SuppressWarnings("unchecked")
-		@Override
-		public void initialize(final UimaContext context)
-				throws ResourceInitializationException {
-			super.initialize(context);
-
-			Class<?> cl;
-			try {
-				cl = Class.forName(className);
-				if (Annotation.class.isAssignableFrom(cl)) {
-					annoClass = (Class<? extends Annotation>) cl;
-				} else {
-					throw new ResourceInitializationException();
-				}
-			} catch (ClassNotFoundException e) {
-				throw new ResourceInitializationException(e);
-			}
-
-		}
-
-		@Override
-		public void process(JCas jcas) throws AnalysisEngineProcessException {
-			int c = 0;
-			for (Annotation anno : JCasUtil.select(jcas, annoClass)) {
-				AnnotationFactory.createAnnotation(jcas, anno.getBegin(),
-						anno.getEnd(), TreeSegment.class).setId(c++);
-			}
-
-		}
 	}
 
 }
