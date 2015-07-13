381ce4cbca934e3501611e1c464c6d9d6a7f1808
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-07-13 10:57:41 +0200

    Handling of new documents

--- de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/rpc/NewDocument.java
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/rpc/NewDocument.java
@@ -1,7 +1,11 @@
 package de.ustu.ims.reiter.treeanno.rpc;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -14,6 +18,17 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.xml.sax.SAXException;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.ustu.ims.reiter.treeanno.CW;
+import de.ustu.ims.reiter.treeanno.DataLayer;
+import de.ustu.ims.reiter.treeanno.beans.Document;
+import de.ustu.ims.reiter.treeanno.beans.Project;
+import de.ustu.ims.reiter.treeanno.util.PlainTextPreprocess;
 
 /**
  * Servlet implementation class NewDocument
@@ -29,6 +44,10 @@ public class NewDocument extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 		if (isMultipart) {
+			File temp = File.createTempFile("treeanno-upload", "");
+			temp.delete();
+			temp.mkdir();
+
 			// Create a factory for disk-based file items
 			DiskFileItemFactory factory = new DiskFileItemFactory();
 
@@ -43,14 +62,35 @@ public class NewDocument extends HttpServlet {
 			// Create a new file upload handler
 			ServletFileUpload upload = new ServletFileUpload(factory);
 
+			DataLayer dbio = CW.getDataLayer(getServletContext());
 			// Parse the request
 			try {
+				Project p = null;
 				List<FileItem> items = upload.parseRequest(request);
-				for (FileItem fitem : items) {
-
-					System.err.println(fitem.getString());
+				for (FileItem fi : items) {
+					if (fi.isFormField()) {
+						String fname = fi.getFieldName();
+						String value = fi.getString();
+						if (fname.equalsIgnoreCase("projectId")) {
+							p = dbio.getProject(Integer.valueOf(value));
+						}
+					} else {
+						IOUtils.copy(fi.getInputStream(), new BufferedWriter(
+								new FileWriter(new File(temp, fi.getName()))));
+					}
 				}
-			} catch (FileUploadException e) {
+				PlainTextPreprocess<Sentence> pp =
+						new PlainTextPreprocess<Sentence>(Sentence.class);
+				Iterator<JCas> iterator = pp.process(temp, "de");
+				while (iterator.hasNext()) {
+					Document document = dbio.getNewDocument(p);
+					dbio.updateJCas(document, iterator.next());
+
+				}
+				response.sendRedirect("projects.jsp");
+				return;
+			} catch (FileUploadException | ResourceInitializationException
+					| SQLException | SAXException e) {
 				throw new ServletException(e);
 			}
 		}
