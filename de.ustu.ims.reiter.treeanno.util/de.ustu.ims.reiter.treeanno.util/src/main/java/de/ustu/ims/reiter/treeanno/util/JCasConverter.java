77fde922f040396d8d2940b6e7947b82db806745
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-08-27 18:41:26 +0200

    new functions to convert JCas to XMI String and back

--- de.ustu.ims.reiter.treeanno.util/de.ustu.ims.reiter.treeanno.util/src/main/java/de/ustu/ims/reiter/treeanno/util/JCasConverter.java
+++ de.ustu.ims.reiter.treeanno.util/de.ustu.ims.reiter.treeanno.util/src/main/java/de/ustu/ims/reiter/treeanno/util/JCasConverter.java
@@ -1,9 +1,23 @@
 package de.ustu.ims.reiter.treeanno.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UIMAException;
+import org.apache.uima.cas.impl.XmiCasDeserializer;
+import org.apache.uima.cas.impl.XmiCasSerializer;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.xml.sax.SAXException;
 
 import de.ustu.ims.reiter.treeanno.api.type.TreeSegment;
 
@@ -31,4 +45,36 @@ public class JCasConverter {
 		obj.put("id", annotation.getId());
 		return obj;
 	}
+
+	public static JCas getJCas(String xmi) throws UIMAException, SAXException,
+			IOException {
+		JCas jcas = null;
+
+		TypeSystemDescription tsd =
+				TypeSystemDescriptionFactory.createTypeSystemDescription();
+		jcas = JCasFactory.createJCas(tsd);
+		InputStream is = null;
+		try {
+			is = new ByteArrayInputStream(xmi.getBytes());
+			XmiCasDeserializer.deserialize(is, jcas.getCas(), true);
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
+		return jcas;
+	}
+
+	public static String getXmi(JCas jcas) throws SAXException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		XmiCasSerializer.serialize(jcas.getCas(), baos);
+
+		String s = null;
+		try {
+			s = new String(baos.toByteArray(), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// This should not happen.
+			e.printStackTrace();
+		}
+		return s;
+	}
+
 }
