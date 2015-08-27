45095644d95236d70435a358813835f6030196d0
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-08-27 19:26:49 +0200

    added static methods to control which fields end up in the json

---
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/JSONUtil.java
@@ -0,0 +1,26 @@
+package de.ustu.ims.reiter.treeanno;
+
+import org.json.JSONObject;
+
+import de.ustu.ims.reiter.treeanno.beans.Document;
+import de.ustu.ims.reiter.treeanno.beans.UserDocument;
+
+public class JSONUtil {
+	public static JSONObject getJSONObject(UserDocument ud) {
+		JSONObject json = new JSONObject();
+		json.put(UserDocument.FIELD_ID, ud.getId());
+		json.put(UserDocument.FIELD_MODIFICATION_DATE, ud.getModificationDate());
+		json.put(UserDocument.FIELD_USER, ud.getUser().getDatabaseId());
+		json.put(UserDocument.FIELD_SRC_DOCUMENT, ud.getDocument()
+				.getDatabaseId());
+		return json;
+	}
+
+	public static JSONObject getJSONObject(Document document) {
+		JSONObject json = new JSONObject();
+		json.put("id", document.getId());
+		json.put("name", document.getName());
+		json.put("project", document.getProject());
+		return json;
+	}
+}
