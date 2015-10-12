e8ddfe2aa5ea2a3019b66a14abcc83b71ac6a39a
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-10-12 14:34:04 +0200

    Class now has externalized strings and getters/setters

--- de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/beans/UserPermission.java
+++ de.ustu.ims.reiter.treeanno/treeanno/src/main/java/de/ustu/ims/reiter/treeanno/beans/UserPermission.java
@@ -5,15 +5,48 @@ import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "treeanno_users_permissions")
 public class UserPermission {
-	@DatabaseField(generatedId = true)
+	public static final String FIELD_ID = "id";
+	public static final String FIELD_USER = "userId";
+	public static final String FIELD_PROJECT = "projectId";
+	public static final String FIELD_LEVEL = "level";
+
+	@DatabaseField(columnName = FIELD_ID, generatedId = true)
 	int id;
 
-	@DatabaseField(foreign = true)
+	@DatabaseField(columnName = FIELD_USER, foreign = true)
 	User userId;
 
-	@DatabaseField(foreign = true)
+	@DatabaseField(columnName = FIELD_PROJECT, foreign = true)
 	Project projectId;
 
-	@DatabaseField
+	@DatabaseField(columnName = FIELD_LEVEL)
 	int level;
+
+	public User getUserId() {
+		return userId;
+	}
+
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
+
+	public Project getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Project projectId) {
+		this.projectId = projectId;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
