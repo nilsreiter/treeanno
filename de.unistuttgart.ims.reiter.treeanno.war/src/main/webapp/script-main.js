4ef79bf463b9debbcd65c8904620aa5114a848e5
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-04-02 14:03:06 +0200

    moved init_parallel from script to script-main

--- de.unistuttgart.ims.reiter.treeanno.war/src/main/webapp/script-main.js
+++ de.unistuttgart.ims.reiter.treeanno.war/src/main/webapp/script-main.js
@@ -424,6 +424,82 @@ function init_main() {
         });
 }
 
+function init_parallel() {
+	init_all();
+	$("#split").hide();
+	$( "button.button_edit_user" ).button({
+		icons: { primary: "ui-icon-person", secondary:null },
+		disabled: true
+	});
+	
+	$( "button.button_change_document" ).button({
+		icons: { primary: "ui-icon-folder-collapsed", secondary:null },
+		//label: i18n.t("open"),
+	}).click(function() {
+		window.location.href="projects.jsp?projectId="+projectId;
+	});
+	$( "button.button_save_document" ).button({
+		icons: { primary: "ui-icon-disk", secondary:null },
+		label: i18n.t("save"),
+	}).click(
+		function() {
+			save_document();
+		}
+	);
+	$("#button_search").button({
+		icons:{primary: "ui-icon-search", secondary: null },
+		label: i18n.t("search")
+	}).click(search);
+	$("#form_search").keyup(search);
+	$("#form_search").focus(function() {enable_interaction=false});
+	$("#form_search").blur(function() {enable_interaction=true});
+	
+	disableSaveButton();
+
+	$(".outline").hide();
+	$("#content > div > .outline").each(function(index, element) {
+		var documentId = userDocumentIds[index];
+		jQuery.getJSON("DocumentContentHandling?userDocumentId="+documentId, function(data) {
+			$(element).parent().prepend("<h2>"+i18n.t("parallel.annotations_from_X",{"user":data["document"]["user"]["name"]})+"</h2>");
+			if (ends_with($(".breadcrumb").text().trim(), ">")) {
+				$(".breadcrumb").append(" <a href=\"projects.jsp?projectId="+data["document"]["document"]["project"]["id"]+"\">"+data["document"]["document"]["project"]["name"]+"</a> &gt; "+i18n.t("parallel.annotations_for_X",{"document":data["document"]["document"]["name"]}));
+				document.title = treeanno["name"]+" "+treeanno["version"]+": "+i18n.t("parallel.annotations_title_for_X",{"document":data["document"]["document"]["name"]});
+			} else {
+				//$(".breadcrumb").append(", "+data["document"]["name"]);
+				// document.title = document.title + ", " + data["document"]["name"];
+			} 
+			
+			var list = data["list"];
+			
+			while (list.length > 0) {
+				var item = list.shift();
+				
+				if ('parentId' in item) {
+					var parentId = item['parentId'];
+					var parentItem = $("li[data-treeanno-id='"+parentId+"']", element);
+					if (parentItem.length == 0)
+						list.push(item);
+					else {
+						if (parentItem.children("ul").length == 0)
+							parentItem.append("<ul></ul>");
+						$("li[data-treeanno-id='"+parentId+"'] > ul", element).append(get_html_item(item, 0));
+					}
+				} else {
+					$(element).append(get_html_item(item, 0));
+				}
+			}
+		
+			$("#status .loading").hide();
+			$(element).show();
+			/*$("li > div", element).smartTruncation({
+			    'truncateCenter'    : true
+			 });*/
+		});
+	});
+	
+}
+
+
 function search() {
 	$("li.searchFound").removeClass("searchFound");
 	var val = $("#form_search").val();
