12d23f5a37cc3252440b07f07563b8eb32142f3b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-04-20 21:04:11 +0200

    ContextListener tries to read additional property settings

--- de.unistuttgart.ims.reiter.treeanno.war/src/main/java/de/ustu/ims/reiter/treeanno/ContextListener.java
+++ de.unistuttgart.ims.reiter.treeanno.war/src/main/java/de/ustu/ims/reiter/treeanno/ContextListener.java
@@ -1,10 +1,11 @@
 package de.ustu.ims.reiter.treeanno;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.SQLException;
-import java.util.Enumeration;
 import java.util.Properties;
 
 import javax.naming.Context;
@@ -49,10 +50,11 @@ public class ContextListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		ServletContext sc = sce.getServletContext();
-		Context envContext;
+		Context envContext = null;
 		try {
 			envContext =
 					(Context) new InitialContext().lookup("java:/comp/env");
+
 			DataSource dataSource =
 					(DataSource) envContext.lookup("jdbc/treeanno");
 			sc.setAttribute("dataSource", dataSource);
@@ -60,22 +62,40 @@ public class ContextListener implements ServletContextListener {
 			e1.printStackTrace();
 		}
 
-		Properties properties = new Properties();
+		Properties properties = null;
+		Properties defaults = new Properties();
+
 		InputStream is = null;
 		try {
+			// reading of default properties from inside the war
 			is = getClass().getResourceAsStream("/project.properties");
-			if (is != null)
-				properties.load(new InputStreamReader(is, "UTF-8"));
-			Enumeration<String> pNames = sc.getInitParameterNames();
-			while (pNames.hasMoreElements()) {
-				String pName = pNames.nextElement();
-				properties.put(pName, sc.getInitParameter(pName));
-			}
+			if (is != null) defaults.load(new InputStreamReader(is, "UTF-8"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			IOUtils.closeQuietly(is);
 		}
+
+		try {
+			// reading additional properties in seperate file, as specified
+			// in the context
+			if (envContext != null) {
+				String path = (String) envContext.lookup("configurationPath");
+				is = new FileInputStream(new File(path));
+				properties = new Properties(defaults);
+				properties.load(new InputStreamReader(is, "UTF-8"));
+			} else {
+				// if no context is found, we take the defaults
+				properties = defaults;
+			}
+		} catch (IOException | NamingException e) {
+			e.printStackTrace();
+			// if something goes wrong, we take the defaults
+			properties = defaults;
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
+
 		for (String s : properties.stringPropertyNames()) {
 			sc.setAttribute(s, properties.get(s));
 		}
@@ -92,5 +112,4 @@ public class ContextListener implements ServletContextListener {
 
 		sc.getSessionCookieConfig().setPath("/");
 	}
-
 }
