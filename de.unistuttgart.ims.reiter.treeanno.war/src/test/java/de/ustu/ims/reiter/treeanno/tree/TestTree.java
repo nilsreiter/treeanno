08c26c3ea329b93be04d9d6d6be100a675bae177
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-04-23 13:00:08 +0200

    more tests

--- de.unistuttgart.ims.reiter.treeanno.war/src/test/java/de/ustu/ims/reiter/treeanno/tree/TestTree.java
+++ de.unistuttgart.ims.reiter.treeanno.war/src/test/java/de/ustu/ims/reiter/treeanno/tree/TestTree.java
@@ -1,19 +1,50 @@
 package de.ustu.ims.reiter.treeanno.tree;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestTree {
+
+	Object[] objects;
+
+	@Before
+	public void setUp() {
+		objects =
+				new Object[] { new Object(), new Object(), new Object(),
+						new Object(), new Object(), new Object(), new Object(),
+						new Object(), new Object() };
+
+	}
+
 	@Test
 	public void testTreeCreation() {
-		Node<String> n = new Node<String>("A");
-		Tree<String> tree = new Tree<String>();
-		tree.setRoot(n);
-		tree.addChild("A", "B");
-		tree.addChild("A", "B");
-		tree.addChild("B", "C");
+		Walker<Object> w = new PrintParenthesesWalker<Object>();
 
-		Walker<String> w = new PrintParenthesesWalker<String>();
+		Tree<Object> tree = new Tree<Object>();
+		tree.setRoot(new Node<Object>(objects[0]));
 		tree.depthFirstWalk(w);
-		System.out.println(w.toString());
+		assertEquals("()", w.toString());
+
+		tree.addChild(objects[0], objects[1]);
+		tree.depthFirstWalk(w);
+		assertEquals("(())", w.toString());
+
+		tree.addChild(objects[0], objects[2]);
+		tree.depthFirstWalk(w);
+		assertEquals("(()())", w.toString());
+
+		tree.addChild(objects[0], objects[4]);
+		tree.depthFirstWalk(w);
+		assertEquals("(()()())", w.toString());
+
+		tree.addChild(objects[1], objects[3]);
+		tree.depthFirstWalk(w);
+		assertEquals("((())()())", w.toString());
+
+		tree.addChild(objects[1], objects[5]);
+		tree.depthFirstWalk(w);
+		assertEquals("((()())()())", w.toString());
 	}
 }
