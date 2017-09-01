af6cf9f7789ffabc8607477a106d3f05b673ebbc
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-09-01 10:49:12 +0200

    javadoc

--- de.unistuttgart.ims.reiter.treeanno.war/src/main/java/de/ustu/ims/reiter/treeanno/tree/Walker.java
+++ de.unistuttgart.ims.reiter.treeanno.war/src/main/java/de/ustu/ims/reiter/treeanno/tree/Walker.java
@@ -1,5 +1,16 @@
 package de.ustu.ims.reiter.treeanno.tree;
 
+/**
+ * A Walker walks over a {@link Tree}. Walker methods are called at certain
+ * points.
+ * 
+ * @author reiterns
+ *
+ * @param <T>
+ *            The Payload type of the tree
+ * @param <R>
+ *            The return type of the walker
+ */
 public interface Walker<T, R> {
 	void init();
 
